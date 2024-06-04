3b25443e4ae6cd6c2c782b93bc47f2bbe6709fc1
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-06-04 15:08:15 +0100

    test(DCMAW-9238): unit test new Base58 class

--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/Base58Test.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/Base58Test.java
@@ -2,26 +2,23 @@ package uk.gov.di.mobile.wallet.cri.credential;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class Base58Test {
 
     @Test
     void shouldDecodeBase58EncodedInput() throws AddressFormatException {
-        assertTrue(
-                Arrays.equals(Base58.decode("MvqLnZTzYSp7V4A"), "Test String".getBytes()),
-                new String(Base58.decode("MvqLnZTzYSp7V4A")));
-        assertTrue(Arrays.equals(Base58.decode("1"), new byte[1]));
-        assertTrue(Arrays.equals(Base58.decode("1111"), new byte[4]));
+        assertArrayEquals("Test String".getBytes(), Base58.decode("MvqLnZTzYSp7V4A"));
+        assertArrayEquals(new byte[1], Base58.decode("1"));
+        assertArrayEquals(new byte[4], Base58.decode("1111"));
     }
 
     @Test
     void shouldReturnAnEmptyByteArrayWhenInputIsAnEmptyString() throws AddressFormatException {
-        assertTrue(Arrays.equals(Base58.decode(""), new byte[0]));
+        assertArrayEquals(new byte[0], Base58.decode(""));
     }
 
     @Test
