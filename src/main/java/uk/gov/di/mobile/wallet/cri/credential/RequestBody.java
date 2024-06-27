957cf4cdca64f36d53440f8b476cecd3f684793f
Ana Hickmann <85736783+anahickmann@users.noreply.github.com>
2024-06-27 11:33:13 +0100

    feat(DCMAW-9365): remove credential_identifier from credential request body (#54)

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/RequestBody.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/RequestBody.java
@@ -8,23 +8,12 @@ public class RequestBody {
     @JsonProperty("proof")
     private Proof proof;
 
-    @JsonProperty("credential_identifier")
-    private String credential_identifier;
-
     @JsonCreator
-    public RequestBody(
-            @JsonProperty(value = "proof", required = true) Proof proof,
-            @JsonProperty(value = "credential_identifier", required = false)
-                    String credentialIdentifier) {
+    public RequestBody(@JsonProperty(value = "proof", required = true) Proof proof) {
         this.proof = proof;
-        this.credential_identifier = credentialIdentifier;
     }
 
     public Proof getProof() {
         return proof;
-    }
-
-    public String getCredentialIdentifier() {
-        return credential_identifier;
     }
 }
