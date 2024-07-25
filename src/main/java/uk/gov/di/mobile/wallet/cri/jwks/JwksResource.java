21314971c6598c34672e5cbfe6211b9d27f286a4
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-07-25 16:38:28 +0100

    feat(DCMAW-9817): implement new jwks resource

--- src/main/java/uk/gov/di/mobile/wallet/cri/jwks/JwksResource.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/jwks/JwksResource.java
@@ -1,50 +1,54 @@
-// package uk.gov.di.mobile.wallet.cri.jwks;
-//
-// import jakarta.inject.Singleton;
-// import jakarta.ws.rs.GET;
-// import jakarta.ws.rs.Path;
-// import jakarta.ws.rs.core.MediaType;
-// import jakarta.ws.rs.core.Response;
-// import org.bouncycastle.openssl.PEMException;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import uk.gov.di.mobile.wallet.cri.did_document.DidDocument;
-// import uk.gov.di.mobile.wallet.cri.did_document.DidDocumentService;
-// import uk.gov.di.mobile.wallet.cri.did_document.KeyNotActiveException;
-//
-// import java.security.NoSuchAlgorithmException;
-//
-// @Singleton
-// @Path("/.well-known/jwks.json")
-// public class JwksResource {
-//
-//    private final DidDocumentService didDocumentService;
-//    private static final Logger LOGGER = LoggerFactory.getLogger(JwksResource.class);
-//
-//    public JwksResource(DidDocumentService didDocumentService) {
-//        this.didDocumentService = didDocumentService;
-//    }
-//
-//    @GET
-//    public Response getJwks() {
-//        try {
-//            DidDocument didDocument = didDocumentService.generateDidDocument();
-//            return buildSuccessResponse().entity(didDocument).build();
-//        } catch (IllegalArgumentException
-//                | PEMException
-//                | NoSuchAlgorithmException
-//                | KeyNotActiveException exception) {
-//            LOGGER.error("An error happened trying to get the DID document: ", exception);
-//            return buildFailResponse().build();
-//        }
-//    }
-//
-//    private Response.ResponseBuilder buildSuccessResponse() {
-//        return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON_TYPE);
-//    }
-//
-//    private Response.ResponseBuilder buildFailResponse() {
-//        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-//                .type(MediaType.APPLICATION_JSON_TYPE);
-//    }
-// }
+package uk.gov.di.mobile.wallet.cri.jwks;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.bouncycastle.openssl.PEMException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.di.mobile.wallet.cri.did_document.KeyNotActiveException;
+import uk.gov.di.mobile.wallet.cri.services.JwksService;
+
+import java.security.NoSuchAlgorithmException;
+
+@Singleton
+@Path("/.well-known/jwks.json")
+public class JwksResource {
+
+    private final JwksService jwksService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwksResource.class);
+
+    public JwksResource(JwksService jwksService) {
+        this.jwksService = jwksService;
+    }
+
+    @GET
+    public Response getJwks() {
+        try {
+            JWKSet jwkSet = jwksService.generateJwks();
+            System.out.println(jwkSet);
+            String jwkSetString = jwkSet.toPublicJWKSet().toString();
+            System.out.println(jwkSetString);
+
+            return buildSuccessResponse().entity(jwkSetString).build();
+        } catch (IllegalArgumentException
+                | PEMException
+                | NoSuchAlgorithmException
+                | KeyNotActiveException exception) {
+            LOGGER.error("An error happened trying to get the DID document: ", exception);
+            return buildFailResponse().build();
+        }
+    }
+
+    private Response.ResponseBuilder buildSuccessResponse() {
+        return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON_TYPE);
+    }
+
+    private Response.ResponseBuilder buildFailResponse() {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .type(MediaType.APPLICATION_JSON_TYPE);
+    }
+}
