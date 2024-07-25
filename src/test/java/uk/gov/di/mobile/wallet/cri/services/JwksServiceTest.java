ea3c783bc1f4cd1944d8c1b4eb509090d8847c8b
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-07-25 16:37:28 +0100

    feat(DCMAW-9817): move JwksService and add new method to create JWKS

---
+++ src/test/java/uk/gov/di/mobile/wallet/cri/services/JwksServiceTest.java
@@ -0,0 +1,138 @@
+package uk.gov.di.mobile.wallet.cri.services;
+
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import org.bouncycastle.openssl.PEMException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.kms.model.DescribeKeyRequest;
+import software.amazon.awssdk.services.kms.model.DescribeKeyResponse;
+import software.amazon.awssdk.services.kms.model.KeyMetadata;
+import uk.gov.di.mobile.wallet.cri.credential.AddressFormatException;
+import uk.gov.di.mobile.wallet.cri.did_document.KeyNotActiveException;
+import uk.gov.di.mobile.wallet.cri.services.signing.KeyProvider;
+import uk.gov.di.mobile.wallet.cri.services.signing.KmsService;
+
+import java.net.MalformedURLException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.ECPublicKey;
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+
+import static com.nimbusds.jose.JWSAlgorithm.ES256;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class JwksServiceTest {
+
+    private JwksService jwksService;
+    private final JWKSource jwkSource = mock(JWKSource.class);
+    private final KeyProvider kmsService = mock(KmsService.class);
+    private final ConfigurationService configurationService = new ConfigurationService();
+    private static final String TEST_ARN =
+            "arn:aws:kms:eu-west-2:00000000000:key/1234abcd-12ab-34cd-56ef-1234567890ab";
+    private static final String TEST_KEY_ID =
+            "d7cb2ed24d8f70433e293ebc270bf1de77fcfab02a7f631da396b70e9b3aa8d7";
+    private static final String TEST_PUBLIC_KEY_TYPE = "EC";
+
+    @Test
+    void shouldReturnJwkWhenFound()
+            throws AddressFormatException, KeySourceException, ParseException {
+        jwksService = new JwksService(configurationService, kmsService, jwkSource);
+        JWK publicKey =
+                JWK.parse(
+                        "{\"kty\":\"EC\",\"crv\":\"P-256\",\"kid\":\"d7cb2ed24d8f70433e293ebc270bf1de77fcfab02a7f631da396b70e9b3aa8d7\",\"x\":\"sSdmBkED2EfjTdX-K2_cT6CfBwXQFt-DJ6v8-6tr_n8\",\"y\":\"WTXmQdqLwrmHN5tiFsTFUtNAvDYhhTQB4zyfteCrWIE\",\"alg\":\"ES256\"}");
+        final List<JWK> jwkList = Collections.singletonList(publicKey);
+        when(jwkSource.get(any(JWKSelector.class), isNull())).thenReturn(jwkList);
+
+        JWK result = jwksService.retrieveJwkFromURLWithKeyId(TEST_KEY_ID);
+
+        assertEquals(publicKey, result);
+    }
+
+    @Test
+    void shouldThrowKeySourceExceptionWhenJwkNotFound()
+            throws AddressFormatException, KeySourceException {
+        jwksService = new JwksService(configurationService, kmsService, jwkSource);
+        final List<JWK> jwkList = Collections.emptyList();
+        when(jwkSource.get(any(JWKSelector.class), isNull())).thenReturn(jwkList);
+
+        KeySourceException exception =
+                assertThrows(
+                        KeySourceException.class,
+                        () -> jwksService.retrieveJwkFromURLWithKeyId(TEST_KEY_ID));
+
+        assertEquals(
+                "No key found with key ID: d7cb2ed24d8f70433e293ebc270bf1de77fcfab02a7f631da396b70e9b3aa8d7",
+                exception.getMessage());
+    }
+
+    @Test
+    void shouldTestAdditionalClassConstructor()
+            throws AddressFormatException, MalformedURLException {
+        jwksService = new JwksService(configurationService, kmsService);
+
+        assertThat(jwksService, instanceOf(JwksService.class));
+    }
+
+    @Test
+    void shouldReturnPublicKeyAsJwks()
+            throws AddressFormatException,
+                    MalformedURLException,
+                    InvalidAlgorithmParameterException,
+                    NoSuchAlgorithmException,
+                    PEMException,
+                    KeyNotActiveException {
+        ECKey mockJwk = getMockJwk();
+        when(kmsService.describeKey(any(DescribeKeyRequest.class)))
+                .thenReturn(getMockDescribeKeyResponse(TEST_ARN, true, null));
+        when(kmsService.isKeyActive(any(String.class))).thenReturn(true);
+        when(kmsService.getPublicKey(any(String.class))).thenReturn(mockJwk);
+
+        JWKSet result = new JwksService(configurationService, kmsService).generateJwks();
+        JWK key = result.getKeyByKeyId(TEST_KEY_ID);
+        ;
+        assertEquals(mockJwk.toString(), key.toJSONString());
+    }
+
+    public static DescribeKeyResponse getMockDescribeKeyResponse(
+            String keyId, boolean enabled, Instant deletionDate) {
+        return DescribeKeyResponse.builder()
+                .keyMetadata(
+                        KeyMetadata.builder()
+                                .keyId(keyId)
+                                .enabled(enabled)
+                                .deletionDate(deletionDate)
+                                .build())
+                .build();
+    }
+
+    private ECKey getMockJwk() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException {
+        KeyPairGenerator gen = KeyPairGenerator.getInstance(TEST_PUBLIC_KEY_TYPE);
+        gen.initialize(Curve.P_256.toECParameterSpec());
+        KeyPair keyPair = gen.generateKeyPair();
+
+        return new ECKey.Builder(Curve.P_256, (ECPublicKey) keyPair.getPublic())
+                .keyID(TEST_KEY_ID)
+                .algorithm(ES256)
+                .build();
+    }
+}
