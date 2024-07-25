3e7749916adb5b95a7e0891e0919d0015dffc9f5
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-07-25 17:32:01 +0100

    chore(DCMAW-9817): fix bug

--- src/main/java/uk/gov/di/mobile/wallet/cri/did_document/DidDocumentService.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/did_document/DidDocumentService.java
@@ -7,13 +7,11 @@ import uk.gov.di.mobile.wallet.cri.services.signing.KeyNotActiveException;
 import uk.gov.di.mobile.wallet.cri.services.signing.KeyProvider;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DidDocumentService {
 
-    private static final List<Did> VERIFICATION_METHOD = new ArrayList<>();
-    private static final List<String> ASSERTION_METHOD = new ArrayList<>();
     private static final String VERIFICATION_METHOD_TYPE = "JsonWebKey2020";
     private static final String CONTROLLER_PREFIX = "did:web:";
     private static final List<String> CONTEXT =
@@ -32,14 +30,14 @@ public class DidDocumentService {
         String keyAlias = configurationService.getSigningKeyAlias();
         String controller = CONTROLLER_PREFIX + configurationService.getDidController();
         Did did = generateDid(keyAlias, controller);
-        VERIFICATION_METHOD.add(did);
-        ASSERTION_METHOD.add(did.getId());
+        List<Did> verificationMethod = Collections.singletonList(did);
+        List<String> assertionMethod = Collections.singletonList(did.getId());
 
         return new DidDocumentBuilder()
                 .setContext(CONTEXT)
                 .setId(controller)
-                .setVerificationMethod(VERIFICATION_METHOD)
-                .setAssertionMethod(ASSERTION_METHOD)
+                .setVerificationMethod(verificationMethod)
+                .setAssertionMethod(assertionMethod)
                 .build();
     }
 
