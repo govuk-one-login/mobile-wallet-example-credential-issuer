e7b4d12108a6e0264508f8c65ee5e0f642a79132
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-08-01 16:15:45 +0100

    Try to get healthcheck working

--- src/main/java/uk/gov/di/mobile/wallet/cri/healthcheck/HealthCheckResource.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/healthcheck/HealthCheckResource.java
@@ -3,6 +3,7 @@ package uk.gov.di.mobile.wallet.cri.healthcheck;
 import com.codahale.metrics.health.HealthCheck;
 
 public class HealthCheckResource extends HealthCheck {
+
     protected Result check() throws Exception {
         return Result.healthy("Application is running");
     }
