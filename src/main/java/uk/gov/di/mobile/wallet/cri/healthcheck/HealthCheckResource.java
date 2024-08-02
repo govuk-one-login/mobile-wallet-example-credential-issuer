01ebf80cca4d3bb63c38193fa3f9fd82f3ab7210
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-08-02 16:02:30 +0100

    Complicate healthcheck

--- src/main/java/uk/gov/di/mobile/wallet/cri/healthcheck/HealthCheckResource.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/healthcheck/HealthCheckResource.java
@@ -1,9 +1,16 @@
 package uk.gov.di.mobile.wallet.cri.healthcheck;
 
+import com.codahale.metrics.health.HealthCheck;
+import io.dropwizard.core.setup.Environment;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.SortedMap;
+import java.util.stream.Collectors;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static jakarta.ws.rs.core.Response.Status.OK;
@@ -11,11 +18,36 @@ import static jakarta.ws.rs.core.Response.Status.OK;
 @Path("/")
 public class HealthCheckResource {
 
+    private final Environment environment;
+
+    public HealthCheckResource(Environment environment) {
+        this.environment = environment;
+    }
+
     @GET
     @Path("healthcheck")
     @Produces(APPLICATION_JSON)
     public Response healthCheck() {
+        SortedMap<String, HealthCheck.Result> results =
+                environment.healthChecks().runHealthChecks();
 
-        return Response.status(OK).entity("Service is available").build();
+        Map<String, Map<String, Object>> response =
+                results.entrySet().stream()
+                        .collect(
+                                Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        healthCheckResult ->
+                                                java.util.Map.of(
+                                                        "healthy",
+                                                        healthCheckResult.getValue().isHealthy(),
+                                                        "message",
+                                                        Objects.toString(
+                                                                healthCheckResult
+                                                                        .getValue()
+                                                                        .getMessage(),
+                                                                "Healthy"))));
+
+        // Always OK as Ping is always healthy
+        return Response.status(OK).entity(response).build();
     }
 }
--- src/test/java/uk/gov/di/mobile/wallet/cri/healthcheck/HealthCheckResourceTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/healthcheck/HealthCheckResourceTest.java
@@ -1,23 +1,55 @@
- package uk.gov.di.mobile.wallet.cri.healthcheck;
- import jakarta.ws.rs.core.Response;
- import org.junit.jupiter.api.BeforeEach;
- import org.junit.jupiter.api.Test;
+package uk.gov.di.mobile.wallet.cri.healthcheck;
 
- import static org.hamcrest.MatcherAssert.assertThat;
- import static org.hamcrest.core.Is.is;
+import com.codahale.metrics.health.HealthCheck;
+import com.codahale.metrics.health.HealthCheckRegistry;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.core.setup.Environment;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
- class HealthCheckResourceTest {
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class HealthCheckResourceTest {
+    @Mock private Environment environment;
+    @Mock private HealthCheckRegistry healthCheckRegistry;
     private HealthCheckResource resource;
 
     @BeforeEach
     void setup() {
-        resource = new HealthCheckResource();
+        when(environment.healthChecks()).thenReturn(healthCheckRegistry);
+        resource = new HealthCheckResource(environment);
     }
 
     @Test
-    void shouldBeHealthyResponseWhenServiceIsHealthy() {
+    void shouldBeHealthyResponseWhenServiceIsHealthy() throws JsonProcessingException {
+        SortedMap<String, HealthCheck.Result> map = new TreeMap<>();
+        map.put("ping", HealthCheck.Result.healthy());
+        map.put("deadlocks", HealthCheck.Result.healthy());
+
+        when(healthCheckRegistry.runHealthChecks()).thenReturn(map);
+
         Response response = resource.healthCheck();
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseBodyString = objectMapper.writeValueAsString(response.getEntity());
+        String expectedResponseBodyString =
+                "{\"ping\":{\"message\":\"Healthy\",\"healthy\":true},\"deadlocks\":{\"message\":\"Healthy\",\"healthy\":true}}";
+        Object responseBody = objectMapper.readValue(responseBodyString, Object.class);
+        Object expectedResponseBody =
+                objectMapper.readValue(expectedResponseBodyString, Object.class);
+
         assertThat(response.getStatus(), is(200));
+        assertThat(responseBody, is(expectedResponseBody));
     }
- }
+}
