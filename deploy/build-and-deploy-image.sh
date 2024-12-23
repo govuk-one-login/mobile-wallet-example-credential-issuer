9f77ce3ccb90b992b523fc442c4ebb5199c0630e
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-12-23 10:39:23 +0000

    feat: remove references to auth stub in bash script

--- deploy/build-and-deploy-image.sh
+++ deploy/build-and-deploy-image.sh
@@ -6,14 +6,14 @@ if [ $# -ge 2 ]
 then
     TAG="$1"
     AWS_PROFILE="$2"
-    echo "Attempting to build docker image for the auth stub with tag: \"$1\""
+    echo "Attempting to build docker image for the Example CRI with tag: \"$1\""
     DOCKER_IMAGE_PATH="671524980203.dkr.ecr.eu-west-2.amazonaws.com/wallet-cred-issuer-be-ecr-containerrepository-ta87rf6hloli:$TAG"
     docker build -t "$DOCKER_IMAGE_PATH" --platform Linux/X86_64 ..
 
     echo "Attempting to log into ECR in the dev account"
     aws ecr get-login-password --region eu-west-2 --profile $AWS_PROFILE | docker login --username AWS --password-stdin 671524980203.dkr.ecr.eu-west-2.amazonaws.com
 
-    echo "Attempting to push image to the auth stub image registry in ECR"
+    echo "Attempting to push image to the Example CRI image registry in ECR"
     docker push "$DOCKER_IMAGE_PATH"
 
     echo "Updating image path in template.yaml to point at the newly pushed image"
@@ -21,5 +21,5 @@ then
 
     echo "Success! You can now build and deploy the SAM template."
 else
-    echo "Please specify a tag for the docker image and your AWS profile for the Mobile Platform dev account, e.g. ./build-and-push-auth-stub-docker-image.sh image-tag profile-name"
+    echo "Please specify a tag for the docker image and your AWS profile for the Mobile Platform dev account, e.g. ./build-and-deploy-image.sh image-tag profile-name"
 fi
