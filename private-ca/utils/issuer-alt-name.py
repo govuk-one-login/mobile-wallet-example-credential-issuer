25957c2d303c212d021ce8e42f03340c28d75bde
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2025-02-28 13:59:01 +0000

    issuer alt name script update and make a URI

--- private-ca/utils/issuer-alt-name.py
+++ private-ca/utils/issuer-alt-name.py
@@ -2,6 +2,25 @@
 
 from cryptography import x509
 import base64
+import argparse
 
-extension = x509.IssuerAlternativeName([x509.RFC822Name("issuer@example.account.gov.uk")])
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--email", type=str)
+parser.add_argument("-u", "--uri", type=str)
+
+args = parser.parse_args()
+
+if not (args.email or args.uri):
+    parser.error('At least one of --email or --uri must be specified')
+
+general_names = []
+
+if args.email:
+    general_names.append(x509.RFC822Name(args.email))
+
+if args.uri:
+    general_names.append(x509.UniformResourceIdentifier(args.uri))
+
+extension = x509.IssuerAlternativeName(general_names)
+
 print(base64.b64encode(extension.public_bytes()))
