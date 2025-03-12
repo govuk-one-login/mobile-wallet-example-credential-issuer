20c68b54ca2d6c507111a3b355d2464400da3282
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-03-12 14:45:43 +0000

    feat: add class to hold Notification request body

---
+++ src/main/java/uk/gov/di/mobile/wallet/cri/notification/RequestBody.java
@@ -0,0 +1,27 @@
+package uk.gov.di.mobile.wallet.cri.notification;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class RequestBody {
+
+    @JsonProperty("notification_id")
+    private String notification_id;
+
+    @JsonProperty("event")
+    private String event;
+
+    @JsonProperty("event_description")
+    private String event_description;
+
+    @JsonCreator
+    public RequestBody(@JsonProperty(value = "notification_id", required = true) String notification_id,
+                       @JsonProperty(value = "event", required = true) String event,
+                       @JsonProperty(value = "event_description") String event_description) {
+        this.notification_id = notification_id;
+        this.event = event;
+        this.event_description = event_description;
+    }
+}
