af11d31010718d41ec1e56e98ffaad3b4a8f475a
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-03-12 14:43:11 +0000

    feat: add No Content and Unauthorized response to ResponseUtil class

--- src/main/java/uk/gov/di/mobile/wallet/cri/util/ResponseUtil.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/util/ResponseUtil.java
@@ -13,13 +13,22 @@ public class ResponseUtil {
         return jsonBuilder(Response.Status.OK, entity).build();
     }
 
+    public static Response noContent() {
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
     public static Response badRequest(String entity) {
         return jsonBuilder(Response.Status.BAD_REQUEST, entity).build();
+    }
+
+    public static Response unauthorized() {
+        return Response.status(Response.Status.UNAUTHORIZED).build();
     }
 
     public static Response internalServerError() {
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
     }
+
 
     private static Response.ResponseBuilder jsonBuilder(Response.Status status, Object entity) {
         return Response.status(status)
