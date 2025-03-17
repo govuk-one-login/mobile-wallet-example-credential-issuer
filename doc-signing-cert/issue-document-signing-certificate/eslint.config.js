62a2a5ea0af698b8e1ee11a615f80319472b8405
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2025-03-17 15:06:56 +0000

    fix linting and prettier

--- doc-signing-cert/issue-document-signing-certificate/eslint.config.js
+++ doc-signing-cert/issue-document-signing-certificate/eslint.config.js
@@ -1,13 +1,38 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+// import globals from 'globals';
+// import pluginJs from '@eslint/js';
+//
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   {
+//     languageOptions: { globals: globals.node },
+//   },
+//   {
+//     ignores: ['node_modules/', '.aws-sam/*'],
+//   },
+//   pluginJs.configs.recommended,
+// ];
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+// eslint.config.js
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
   {
-    languageOptions: { globals: globals.node },
-  },
-  {
+    rules: {
+      semi: 'error',
+      'prefer-const': 'error',
+    },
     ignores: ['node_modules/', '.aws-sam/*'],
   },
-  pluginJs.configs.recommended,
-];
+]);
+
+// eslint.config.js
+// import { defineConfig } from "eslint/config";
+//
+// export default defineConfig([
+//   {
+//     rules: {
+//       semi: "error",
+//       "prefer-const": "error"
+//     }
+//   }
+// ]);
