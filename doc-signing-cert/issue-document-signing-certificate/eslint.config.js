3460d7ed1d216354d86dfb0e229f874536314670
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2025-03-19 12:46:46 +0000

    more eslint and prettier config

--- doc-signing-cert/issue-document-signing-certificate/eslint.config.js
+++ doc-signing-cert/issue-document-signing-certificate/eslint.config.js
@@ -5,19 +5,14 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-// );
-// // export default defineConfig([
+  // );
+  // // export default defineConfig([
   {
     rules: {
       semi: 'error',
       'prefer-const': 'error',
     },
-    ignores: [
-      'node_modules/**/*',
-      '.aws-sam/**/*',
-      'coverage/**/*',
-      '.prettierrc.cjs',
-    ],
+    ignores: ['node_modules/**/*', '.aws-sam/**/*', 'coverage/**/*', '.prettierrc.cjs'],
   },
 );
 // ]);
