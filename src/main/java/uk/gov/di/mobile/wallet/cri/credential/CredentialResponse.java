1f5ebe23fac1f0ec8e2d96e3b9391aceb138075e
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-03-19 17:33:42 +0000

    fix: adding JsonProperty to getNotificationId instead of the data field

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/CredentialResponse.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/CredentialResponse.java
@@ -5,13 +5,16 @@ import lombok.Getter;
 
 @Getter
 public class CredentialResponse {
-    private final String credential;
-
-    @JsonProperty("notification_id")
-    private final String notificationId;
+    private final String credential; // NOSONAR
+    private final String notificationId; // NOSONAR
 
     public CredentialResponse(String credential, String notificationId) {
         this.credential = credential;
         this.notificationId = notificationId;
+    }
+
+    @JsonProperty("notification_id")
+    public String getNotificationId() {
+        return notificationId;
     }
 }
