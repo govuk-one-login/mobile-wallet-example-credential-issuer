9d32d3f2d3a62b1085fb02a2288ec8ba5466a3aa
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-03-20 11:26:44 +0000

    DCMAW-11867: Add notification endpoint to metadata (#183)
    * feat: add notification endpoint to CRI metadata
    
    * test: notification endpoint in metadata
    
    * feat: fix code smell
    
    * refactor: update property names to remove NOSONAR comment

--- src/main/java/uk/gov/di/mobile/wallet/cri/metadata/Metadata.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/metadata/Metadata.java
@@ -9,47 +9,53 @@ public class Metadata {
             String authorizationServers,
             String credentialsEndpoint,
             String credentialEndpoint,
+            String notificationEndpoint,
             Object credentialConfigurationsSupported) {
-        this.credential_issuer = credentialIssuer;
-        this.authorization_servers = new String[] {authorizationServers};
-
-        this.credentials_endpoint = credentialsEndpoint;
+        this.credentialIssuer = credentialIssuer;
+        this.authorizationServers = new String[] {authorizationServers};
         // TO DO: Remove credentials_endpoint once SDK has been updated:
         // https://govukverify.atlassian.net/browse/DCMAW-11040
         // https://govukverify.atlassian.net/browse/DCMAW-11043
-
-        this.credential_endpoint = credentialEndpoint;
-        this.credential_configurations_supported = credentialConfigurationsSupported;
+        this.credentialsEndpoint = credentialsEndpoint;
+        this.credentialEndpoint = credentialEndpoint;
+        this.notificationEndpoint = notificationEndpoint;
+        this.credentialConfigurationsSupported = credentialConfigurationsSupported;
     }
 
-    String credential_issuer; // NOSONAR
-    String[] authorization_servers; // NOSONAR
-    String credentials_endpoint; // NOSONAR
-    String credential_endpoint; // NOSONAR
-    Object credential_configurations_supported; // NOSONAR
+    String credentialIssuer; // NOSONAR
+    String[] authorizationServers; // NOSONAR
+    String credentialsEndpoint; // NOSONAR
+    String credentialEndpoint; // NOSONAR
+    String notificationEndpoint; // NOSONAR
+    Object credentialConfigurationsSupported; // NOSONAR
 
-    @JsonProperty("credentials_endpoint")
-    public String getCredentialsEndpoint() {
-        return credentials_endpoint;
-    }
-
-    @JsonProperty("credential_endpoint")
-    public String getCredentialEndpoint() {
-        return credential_endpoint;
+    @JsonProperty("credential_issuer")
+    public String getCredentialIssuer() {
+        return credentialIssuer;
     }
 
     @JsonProperty("authorization_servers")
     public String[] getAuthorizationServers() {
-        return authorization_servers;
+        return authorizationServers;
     }
 
-    @JsonProperty("credential_issuer")
-    public String getCredentialIssuer() {
-        return credential_issuer;
+    @JsonProperty("credentials_endpoint")
+    public String getCredentialsEndpoint() {
+        return credentialsEndpoint;
+    }
+
+    @JsonProperty("credential_endpoint")
+    public String getCredentialEndpoint() {
+        return credentialEndpoint;
+    }
+
+    @JsonProperty("notification_endpoint")
+    public String getNotificationEndpoint() {
+        return notificationEndpoint;
     }
 
     @JsonProperty("credential_configurations_supported")
     public Object getCredentialsSupported() {
-        return credential_configurations_supported;
+        return credentialConfigurationsSupported;
     }
 }
