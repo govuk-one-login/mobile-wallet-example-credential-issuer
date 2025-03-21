63d5fbe96daba9c4239d7432110e2cd478d51591
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-03-21 16:04:41 +0000

    feat: remove obsolete credentials_endpoint

--- src/main/java/uk/gov/di/mobile/wallet/cri/metadata/Metadata.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/metadata/Metadata.java
@@ -7,16 +7,11 @@ public class Metadata {
     public Metadata(
             String credentialIssuer,
             String authorizationServers,
-            String credentialsEndpoint,
             String credentialEndpoint,
             String notificationEndpoint,
             Object credentialConfigurationsSupported) {
         this.credentialIssuer = credentialIssuer;
         this.authorizationServers = new String[] {authorizationServers};
-        // TO DO: Remove credentials_endpoint once SDK has been updated:
-        // https://govukverify.atlassian.net/browse/DCMAW-11040
-        // https://govukverify.atlassian.net/browse/DCMAW-11043
-        this.credentialsEndpoint = credentialsEndpoint;
         this.credentialEndpoint = credentialEndpoint;
         this.notificationEndpoint = notificationEndpoint;
         this.credentialConfigurationsSupported = credentialConfigurationsSupported;
@@ -24,7 +19,6 @@ public class Metadata {
 
     String credentialIssuer; // NOSONAR
     String[] authorizationServers; // NOSONAR
-    String credentialsEndpoint; // NOSONAR
     String credentialEndpoint; // NOSONAR
     String notificationEndpoint; // NOSONAR
     Object credentialConfigurationsSupported; // NOSONAR
@@ -37,11 +31,6 @@ public class Metadata {
     @JsonProperty("authorization_servers")
     public String[] getAuthorizationServers() {
         return authorizationServers;
-    }
-
-    @JsonProperty("credentials_endpoint")
-    public String getCredentialsEndpoint() {
-        return credentialsEndpoint;
     }
 
     @JsonProperty("credential_endpoint")
