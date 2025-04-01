748e8c97bcbc1178e1569b09885efb1eb5b465b7
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-01 09:58:24 +0100

    feat: add DigestIDGenerator class

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGenerator.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGenerator.java
@@ -1,18 +1,15 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class DigestIDGenerator {
   private int number;
-  private Random random;
-
   private static final int LOWER_BOUND = 0;
   private static final int UPPER_BOUND = (int) Math.pow(2, 30);
 
-  // Constructor to set a random starting number within the lower and upper bounds
   public DigestIDGenerator() {
-    this.random = new Random();
-    this.number = random.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+    SecureRandom secureRandom = new SecureRandom();
+    this.number = secureRandom.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
   }
 
   public int next() {
