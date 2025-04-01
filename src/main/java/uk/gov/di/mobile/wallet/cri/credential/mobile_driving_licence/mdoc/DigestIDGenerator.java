69bc1ed73625e62cb736791c8b384117b52698b8
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-01 16:57:41 +0100

    style: format code

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGenerator.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGenerator.java
@@ -4,30 +4,30 @@ import java.security.SecureRandom;
 
 /**
  * Generates sequential integer IDs within a defined range.
- * <p>
- * This class uses a cryptographically secure random number generator
- * to initialize the starting point of the sequence.
+ *
+ * <p>This class uses a cryptographically secure random number generator to initialize the starting
+ * point of the sequence.
  */
 public class DigestIDGenerator {
-  private int number;
-  private static final int LOWER_BOUND = 0;
-  private static final int UPPER_BOUND = (int) Math.pow(2, 30);
+    private int number;
+    private static final int LOWER_BOUND = 0;
+    private static final int UPPER_BOUND = (int) Math.pow(2, 30);
 
-  /**
-   * Constructs a DigestIDGenerator with a random starting number
-   * within the defined {@link #LOWER_BOUND} and {@link #UPPER_BOUND}.
-   */
-  public DigestIDGenerator() {
-    SecureRandom secureRandom = new SecureRandom();
-    this.number = secureRandom.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
-  }
+    /**
+     * Constructs a DigestIDGenerator with a random starting number within the defined {@link
+     * #LOWER_BOUND} and {@link #UPPER_BOUND}.
+     */
+    public DigestIDGenerator() {
+        SecureRandom secureRandom = new SecureRandom();
+        this.number = secureRandom.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+    }
 
-  /**
-   * Generates the next sequential ID in the sequence.
-   *
-   * @return The next integer ID.
-   */
-  public int next() {
-    return ++number;
-  }
+    /**
+     * Generates the next sequential ID in the sequence.
+     *
+     * @return The next integer ID.
+     */
+    public int next() {
+        return ++number;
+    }
 }
--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/MdlEncodingException.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/MdlEncodingException.java
@@ -2,7 +2,7 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
 public class MdlEncodingException extends RuntimeException {
 
-  public MdlEncodingException(String message, Exception cause) {
-    super(message, cause);
-  }
+    public MdlEncodingException(String message, Exception cause) {
+        super(message, cause);
+    }
 }
