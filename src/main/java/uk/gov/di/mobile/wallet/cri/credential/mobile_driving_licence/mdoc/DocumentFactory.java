6d6c4097938cb51013da24bd89936b856259285d
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-01 13:01:25 +0100

    feat: create DocumentFactory class

---
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DocumentFactory.java
@@ -0,0 +1,51 @@
+package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
+
+import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.DrivingLicenceDocument;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DocumentFactory {
+  private static final String MOBILE_DRIVING_LICENCE_NAMESPACE = "org.iso.18013.5.1";
+  private static final String MOBILE_DRIVING_LICENCE_DOCUMENT_TYPE = MOBILE_DRIVING_LICENCE_NAMESPACE + "mDL";
+
+  private final IssuerSignedItemFactory issuerSignedItemFactory;
+
+  public DocumentFactory(IssuerSignedItemFactory issuerSignedItemFactory) {
+    this.issuerSignedItemFactory = issuerSignedItemFactory;
+  }
+
+  public Document build(final DrivingLicenceDocument drivingLicence) {
+    Map<String, List<IssuerSignedItem>> nameSpaces = buildNameSpaces(drivingLicence);
+    IssuerSigned issuerSigned = buildIssuerSigned(nameSpaces);
+    return new Document(MOBILE_DRIVING_LICENCE_DOCUMENT_TYPE, issuerSigned);
+  }
+
+  private Map<String, List<IssuerSignedItem>> buildNameSpaces(final DrivingLicenceDocument drivingLicence) {
+    try {
+      List<IssuerSignedItem> issuerSignedItems = new ArrayList<>();
+      for (Field field : drivingLicence.getClass().getDeclaredFields()) {
+        String fieldName = field.getName();
+
+        String asSnakeCase = CamelToSnakeCaseConvertor.convert(fieldName);
+        Object fieldValue = field.get(drivingLicence);
+
+        IssuerSignedItem issuerSignedItem =
+                issuerSignedItemFactory.build(
+                        asSnakeCase, fieldValue);
+        issuerSignedItems.add(issuerSignedItem);
+      }
+
+      return Map.of(MOBILE_DRIVING_LICENCE_NAMESPACE, issuerSignedItems);
+    } catch (IllegalAccessException exception) {
+      throw new MdlEncodingException(
+              "Error occurred when accessing Driving Licence to build Name", exception);
+    }
+  }
+
+  private IssuerSigned buildIssuerSigned(final Map<String, List<IssuerSignedItem>> nameSpaces) {
+    return new IssuerSigned(nameSpaces, new IssuerAuth());
+  }
+}
