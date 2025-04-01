63bdb752368d6c46748c5070fdc73065f888dfff
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-01 12:21:58 +0100

    feat: create MdlEncodingException class

---
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/MdlEncodingException.java
@@ -0,0 +1,8 @@
+package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
+
+public class MdlEncodingException extends RuntimeException {
+
+  public MdlEncodingException(String message, Exception cause) {
+    super(message, cause);
+  }
+}
