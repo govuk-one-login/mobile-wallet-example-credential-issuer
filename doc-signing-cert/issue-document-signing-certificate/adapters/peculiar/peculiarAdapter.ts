fb6c8a342fe6d4d15a90a8bedfb27dcb431d7263
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2025-04-15 20:30:10 +0100

    refactor peculiarAdapter.ts per PR comment

--- doc-signing-cert/issue-document-signing-certificate/adapters/peculiar/peculiarAdapter.ts
+++ doc-signing-cert/issue-document-signing-certificate/adapters/peculiar/peculiarAdapter.ts
@@ -17,37 +17,28 @@ export async function createCertificateRequestFromEs256KmsKey(
 ) {
   const spki = await getPublicKey(keyId);
 
-  const asnReq = new CertificationRequest({
-    certificationRequestInfo: new CertificationRequestInfo({
-      subjectPKInfo: AsnConvert.parse(spki, SubjectPublicKeyInfo),
-    }),
-  });
-
   const name = csrName instanceof Name ? csrName : new Name(csrName);
-  asnReq.certificationRequestInfo.subject = AsnConvert.parse(name.toArrayBuffer(), AsnName);
-
+  const certificationRequestInfo = new CertificationRequestInfo({
+    subjectPKInfo: AsnConvert.parse(spki, SubjectPublicKeyInfo),
+    subject: AsnConvert.parse(name.toArrayBuffer(), AsnName),
+  });
+  const tbs = AsnConvert.serialize(certificationRequestInfo);
+  const signature = await signWithEcdsaSha256(keyId, tbs);
   const signingAlgorithm = {
     name: 'ECDSA',
     namedCurve: 'P-256',
     hash: 'SHA-256',
   };
-
-  const algProv = new AlgorithmProvider();
-  asnReq.signatureAlgorithm = algProv.toAsnAlgorithm(signingAlgorithm);
-
-  const tbs = AsnConvert.serialize(asnReq.certificationRequestInfo);
-  const signature = await signWithEcdsaSha256(keyId, tbs);
-
-  const signatureFormatter = new AsnEcSignatureFormatter();
-  const asnSignature = signatureFormatter.toAsnSignature(signingAlgorithm, signature);
-
+  const asnSignature = new AsnEcSignatureFormatter().toAsnSignature(signingAlgorithm, signature);
   if (!asnSignature) {
-    throw Error('Cannot convert WebCrypto signature value to ASN.1 format');
+    throw new Error('Cannot convert WebCrypto signature value to ASN.1 format');
   }
-
-  asnReq.signature = asnSignature;
-
-  return new Pkcs10CertificateRequest(AsnConvert.serialize(asnReq));
+  const asnRequest = new CertificationRequest({
+    certificationRequestInfo,
+    signatureAlgorithm: new AlgorithmProvider().toAsnAlgorithm(signingAlgorithm),
+    signature: asnSignature,
+  });
+  return new Pkcs10CertificateRequest(AsnConvert.serialize(asnRequest));
 }
 
 export function decodeX509Certificate(certificatePem: string): X509Certificate {
