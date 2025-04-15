64bfc02eb3762615e3fc85fd4bb5441b10df3713
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-15 17:35:41 +0100

    feat: update IssuerSigned to contain an array of bytes

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/IssuerSigned.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/IssuerSigned.java
@@ -3,4 +3,4 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 import java.util.List;
 import java.util.Map;
 
-public record IssuerSigned(Map<String, List<IssuerSignedItem>> nameSpaces, IssuerAuth issuerAuth) {}
+public record IssuerSigned(Map<String, List<byte[]>> nameSpaces, IssuerAuth issuerAuth) {}
