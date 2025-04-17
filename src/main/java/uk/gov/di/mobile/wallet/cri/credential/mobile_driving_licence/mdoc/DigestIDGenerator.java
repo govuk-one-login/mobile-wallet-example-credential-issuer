3ba80acd7c939822ad6b56fc32f3da4387ac721d
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-17 16:25:23 +0100

    feat: make increment random but within bounds

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGenerator.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGenerator.java
@@ -3,31 +3,48 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 import java.security.SecureRandom;
 
 /**
- * Generates sequential integer IDs within a defined range.
+ * Generates sequential numeric IDs.
  *
- * <p>This class uses a cryptographically secure random number generator to initialize the starting
- * point of the sequence.
+ * <p>This class generates sequential numeric IDs within a defined range. The ID generation process
+ * ensures that each generated ID is unique within the bounds of the generator's configuration.
+ *
+ * <p>The generator starts with a randomly initialized 'number' and an 'increment', both within
+ * predefined bounds. Each call to the {@link #next()} method calculates the next ID in the sequence
+ * by adding the 'increment' to the current 'number'.
  */
 public class DigestIDGenerator {
     private int number;
-    private static final int LOWER_BOUND = 0;
-    private static final int UPPER_BOUND = (int) Math.pow(2, 30);
+    private final int increment;
+    private static final int NUMBER_LOWER_BOUND = 0;
+    private static final int NUMBER_ID_UPPER_BOUND = (int) Math.pow(2, 30);
+    private static final int INCREMENT_LOWER_BOUND = 1;
+    private static final int INCREMENT_UPPER_BOUND = 99;
 
     /**
-     * Constructs a DigestIDGenerator with a random starting number within the defined {@link
-     * #LOWER_BOUND} and {@link #UPPER_BOUND}.
+     * Constructs a DigestIDGenerator with a random starting number and increment.
+     *
+     * <p>The starting number is randomly generated within the range defined by {@link
+     * #NUMBER_LOWER_BOUND} and {@link #NUMBER_ID_UPPER_BOUND}. The increment is randomly generated
+     * within the range defined by {@link #INCREMENT_LOWER_BOUND} and {@link
+     * #INCREMENT_UPPER_BOUND}.
      */
     public DigestIDGenerator() {
         SecureRandom secureRandom = new SecureRandom();
-        this.number = secureRandom.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+        this.number =
+                secureRandom.nextInt(NUMBER_ID_UPPER_BOUND - NUMBER_LOWER_BOUND)
+                        + NUMBER_LOWER_BOUND;
+        this.increment =
+                secureRandom.nextInt(INCREMENT_UPPER_BOUND - INCREMENT_LOWER_BOUND)
+                        + INCREMENT_LOWER_BOUND;
     }
 
     /**
-     * Generates the next sequential ID in the sequence.
+     * Generates the next integer ID in the sequence.
      *
-     * @return The next integer ID.
+     * @return The next integer ID in the sequence.
      */
     public int next() {
-        return ++number;
+        number = number + increment;
+        return number;
     }
 }
--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGeneratorTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DigestIDGeneratorTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class DigestIDGeneratorTest {
 
     @Test
-    void Should_InitializeConstructorWithRandomValue() {
+    void Should_InitializeConstructorWithRandomStartingValue() {
         DigestIDGenerator generator1 = new DigestIDGenerator();
         DigestIDGenerator generator2 = new DigestIDGenerator();
         DigestIDGenerator generator3 = new DigestIDGenerator();
@@ -21,11 +21,29 @@ class DigestIDGeneratorTest {
 
         assertFalse(
                 value1 == value2 && value2 == value3,
-                "Multiple generators should initialize with different values");
+                "Different generators should initialize with different starting values");
     }
 
     @Test
-    void Should_IncrementValue_When_NextMethodIsCalled() {
+    void Should_InitializeConstructorWithRandomIncrement() {
+        DigestIDGenerator generator1 = new DigestIDGenerator();
+        DigestIDGenerator generator2 = new DigestIDGenerator();
+        DigestIDGenerator generator3 = new DigestIDGenerator();
+
+        int firstValue1 = generator1.next() - generator1.next();
+        int increment1 = generator1.next() - firstValue1;
+        int firstValue2 = generator2.next() - generator2.next();
+        int increment2 = generator2.next() - firstValue2;
+        int firstValue3 = generator3.next() - generator3.next();
+        int increment3 = generator3.next() - firstValue3;
+
+        assertFalse(
+                increment1 == increment2 && increment2 == increment3,
+                "Different generators should initialize with different increment values");
+    }
+
+    @Test
+    void Should_GenerateSequentialIdsWithRandomIncrement_When_NextMethodIsCalled() {
         DigestIDGenerator generator = new DigestIDGenerator();
 
         int firstValue = generator.next();
@@ -33,18 +51,37 @@ class DigestIDGeneratorTest {
         int thirdValue = generator.next();
         int fourthValue = generator.next();
         int fifthValue = generator.next();
+        int sixthValue = generator.next();
+        int increment = secondValue - firstValue;
 
-        assertEquals(firstValue + 1, secondValue, "Second value should be first value + 1");
-        assertEquals(secondValue + 1, thirdValue, "Third value should be second value + 1");
-        assertEquals(thirdValue + 1, fourthValue, "Fourth value should be third value + 1");
-        assertEquals(fourthValue + 1, fifthValue, "Fifth value should be fourth value + 1");
+        assertEquals(
+                firstValue + increment,
+                secondValue,
+                "Second value should be first value + 'increment'");
+        assertEquals(
+                secondValue + increment,
+                thirdValue,
+                "Third value should be second value + 'increment'");
+        assertEquals(
+                thirdValue + increment,
+                fourthValue,
+                "Fourth value should be third value + 'increment'");
+        assertEquals(
+                fourthValue + increment,
+                fifthValue,
+                "Fifth value should be fourth value + 'increment'");
+        assertEquals(
+                fifthValue + increment,
+                sixthValue,
+                "Sixth value should be fifth value + 'increment'");
     }
 
-    @RepeatedTest(10)
+    @RepeatedTest(20)
     void Should_GenerateInitialValueWithinBounds() {
         DigestIDGenerator generator = new DigestIDGenerator();
-        int initialValuePlusOne = generator.next();
-        int initialValue = initialValuePlusOne - 1;
+        int initialValuePlusIncrement = generator.next();
+        int increment = generator.next() - initialValuePlusIncrement;
+        int initialValue = initialValuePlusIncrement - increment;
 
         assertTrue(initialValue >= 0, "Initial value should be greater than or equal to 0");
         assertTrue(initialValue < Math.pow(2, 30), "Initial value should be less than 2^30");
