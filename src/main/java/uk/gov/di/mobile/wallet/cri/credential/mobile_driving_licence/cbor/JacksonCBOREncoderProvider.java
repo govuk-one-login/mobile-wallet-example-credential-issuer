862b64d167526988ab478ee0426959ce916b8625
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-18 13:19:58 +0100

    chore: annotate class to exclude it from coverage report

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cbor/JacksonCBOREncoderProvider.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cbor/JacksonCBOREncoderProvider.java
@@ -2,6 +2,7 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cbor;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper;
+import uk.gov.di.mobile.wallet.cri.annotations.ExcludeFromGeneratedCoverageReport;
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.IssuerSigned;
 
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 /** Provides a pre-configured {@link CBORMapper} instance with custom serializers. */
 public final class JacksonCBOREncoderProvider {
 
+    @ExcludeFromGeneratedCoverageReport
     private JacksonCBOREncoderProvider() {
         throw new IllegalStateException("Instantiation is not valid for this class.");
     }
