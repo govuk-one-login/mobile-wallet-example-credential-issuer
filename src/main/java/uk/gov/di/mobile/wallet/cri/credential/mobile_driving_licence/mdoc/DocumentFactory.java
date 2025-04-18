e6afeeb70c53b758a5eb14a60c0f0243c5feb2f7
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-04-18 12:07:23 +0100

    chore: remove empty constructor

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DocumentFactory.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DocumentFactory.java
@@ -8,8 +8,6 @@ import java.util.Map;
 public class DocumentFactory {
     private static final String MOBILE_DRIVING_LICENCE_DOCUMENT_TYPE = "org.iso.18013.5.1.mDL";
 
-    public DocumentFactory() {}
-
     public Document build(final Map<String, List<byte[]>> nameSpaces) throws MDLException {
         IssuerSigned issuerSigned = buildIssuerSigned(nameSpaces);
         return new Document(MOBILE_DRIVING_LICENCE_DOCUMENT_TYPE, issuerSigned);
