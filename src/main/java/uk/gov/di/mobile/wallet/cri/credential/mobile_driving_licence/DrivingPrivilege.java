5a1741db78a07f892ac46031a1a8fac6a52647fe
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-05-15 11:17:29 +0100

    feat: make issue and expiry date properties optional

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/DrivingPrivilege.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/DrivingPrivilege.java
@@ -6,29 +6,30 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Setter
 @Getter
 public class DrivingPrivilege {
-  @JsonProperty("vehicle_category_code")
-  private String vehicleCategoryCode;
+    @JsonProperty("vehicle_category_code")
+    private String vehicleCategoryCode;
 
-  @JsonProperty(value = "issue_date", required = false)
-  private LocalDate issueDate;
+    @JsonProperty(value = "issue_date")
+    private Optional<LocalDate> issueDate;
 
-  @JsonProperty(value = "expiry_date", required = false)
-  private LocalDate expiryDate;
+    @JsonProperty(value = "expiry_date")
+    private Optional<LocalDate> expiryDate;
 
-  public void setIssueDate(String issueDate) {
-    this.issueDate = parseDate(issueDate);
-  }
+    public void setIssueDate(String issueDate) {
+        this.issueDate = Optional.of(parseDate(issueDate));
+    }
 
-  public void setExpiryDate(String expiryDate) {
-    this.expiryDate = parseDate(expiryDate);
-  }
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = Optional.of(parseDate(expiryDate));
+    }
 
-  private LocalDate parseDate(String dateString) {
-    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    return LocalDate.parse(dateString, dateFormat);
-  }
+    private LocalDate parseDate(String dateString) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return LocalDate.parse(dateString, dateFormat);
+    }
 }
