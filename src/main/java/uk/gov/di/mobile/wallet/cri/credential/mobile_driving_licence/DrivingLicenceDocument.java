c4efca6c55c4cf3ed4991417e419c535e50e4018
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-05-20 16:20:50 +0100

    feat: remove @JsonInclude(JsonInclude.Include.NON_ABSENT) as not working as expected

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/DrivingLicenceDocument.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/DrivingLicenceDocument.java
@@ -1,7 +1,6 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
