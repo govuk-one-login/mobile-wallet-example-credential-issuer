3cc0c13590e5e181368d8383beb9e491ad9f218c
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-05-27 18:32:06 +0100

    feat: add MSO to IssuerAuth record

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/IssuerAuth.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/IssuerAuth.java
@@ -1,5 +1,3 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
-public record IssuerAuth() { // NOSONAR
-    // Functionality will be implemented in a separate Jira ticket
-}
+public record IssuerAuth(MobileSecurityObject mobileSecurityObject) {}
