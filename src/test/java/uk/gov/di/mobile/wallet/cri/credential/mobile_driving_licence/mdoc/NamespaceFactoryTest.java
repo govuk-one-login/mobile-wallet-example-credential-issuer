c58b6a0d52b21c6a728e7fb6bd582474ad8980bf
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-05-29 17:49:42 +0100

    fix: Add UK annotation to title to be included in the UK namespace and fix tests

--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/NamespaceFactoryTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/NamespaceFactoryTest.java
@@ -25,8 +25,8 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class NamespaceFactoryTest {
 
-    private static final int EXPECTED_ISO_FIELDS = 16;
-    private static final int EXPECTED_UK_FIELDS = 1;
+    private static final int EXPECTED_ISO_FIELDS = 15;
+    private static final int EXPECTED_UK_FIELDS = 2;
     private static final byte[] MOCK_CBOR_BYTES = {0x01, 0x02};
     private static final DrivingPrivilege[] DRIVING_PRIVILEGES = {
         new DrivingPrivilege("B", null, null)
@@ -73,7 +73,6 @@ class NamespaceFactoryTest {
         isoNamespace.forEach(bytes -> assertArrayEquals(MOCK_CBOR_BYTES, bytes));
         verify(mockIssuerSignedItemFactory).build("family_name", "Doe");
         verify(mockIssuerSignedItemFactory).build("given_name", "John");
-        verify(mockIssuerSignedItemFactory).build("title", "Miss");
         verify(mockIssuerSignedItemFactory).build("portrait", "base64EncodedPortraitString");
         verify(mockIssuerSignedItemFactory).build("birth_date", LocalDate.parse("1985-05-24"));
         verify(mockIssuerSignedItemFactory).build("birth_place", "London");
@@ -102,6 +101,7 @@ class NamespaceFactoryTest {
         ukNamespace.forEach(bytes -> assertArrayEquals(MOCK_CBOR_BYTES, bytes));
         verify(mockIssuerSignedItemFactory)
                 .build("provisional_driving_privileges", Optional.ofNullable(DRIVING_PRIVILEGES));
+        verify(mockIssuerSignedItemFactory).build("title", "Miss");;
     }
 
     @Test
