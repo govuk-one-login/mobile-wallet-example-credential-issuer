5516d2eedec36166061e1fe43bd81f344ff2f8b9
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-02 12:16:51 +0100

    feat: return HashMap from valueDigests method

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/ValueDigests.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/ValueDigests.java
@@ -2,6 +2,7 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,10 +14,19 @@ import java.util.Map;
  * <p>For example, the namespace {@code "org.iso.18013.5.1"} could map to a child map like: {@code 0
  * -> byte[], 5 -> byte[]}, representing two digest IDs and their serialized digest values.
  */
-public record ValueDigests(Map<String, Map<Integer, byte[]>> namespaceToValueDigests) {
+public record ValueDigests(Map<String, Map<Integer, byte[]>> valueDigests) {
 
+    /**
+     * Returns a shallow copy of the mapping from namespaces to their digest ID and digest value
+     * maps.
+     * <p>
+     * The {@link JsonValue} annotation ensures that when this object is serialized with Jackson,
+     * the returned map is used directly, omitting the record's field name from the output.
+     * </p>
+     * @return A shallow copy of the namespace to digest mappings.
+     */
     @JsonValue
-    public Map<String, Map<Integer, byte[]>> value() {
-      return namespaceToValueDigests;
+    public Map<String, Map<Integer, byte[]>> valueDigests() {
+        return new HashMap<>(valueDigests);
     }
 }
