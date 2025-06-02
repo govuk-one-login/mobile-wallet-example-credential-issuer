17221da2e5a9247eaa4369e9e58307e0782f767f
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-02 17:12:06 +0100

    test: update MobileDrivingLicenceServiceTest tests

--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/MobileDrivingLicenceServiceTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/MobileDrivingLicenceServiceTest.java
@@ -10,8 +10,6 @@ import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cbor.MDLExc
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.Document;
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.DocumentFactory;
 
-import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -22,53 +20,38 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class MobileDrivingLicenceServiceTest {
 
-    private static final String MOCK_NAMESPACE_1 = "mockNamespace1";
-    private static final String MOCK_NAMESPACE_2 = "mockNamespace2";
-    private static final String EXPECTED_HEX = "a10102";
-
     @Mock private CBOREncoder cborEncoder;
     @Mock private DocumentFactory documentFactory;
-    @Mock private DocumentFactory documentFactory;
     @Mock private DrivingLicenceDocument mockDrivingLicenceDocument;
-    @Mock private Document mockMdoc;
+    @Mock private Document mockDocument;
 
     private MobileDrivingLicenceService mobileDrivingLicenceService;
 
     @BeforeEach
     void setUp() {
-        mobileDrivingLicenceService =
-                new MobileDrivingLicenceService(cborEncoder, documentFactory, documentFactory);
+        mobileDrivingLicenceService = new MobileDrivingLicenceService(cborEncoder, documentFactory);
     }
 
     @Test
     void Should_CreateMobileDrivingLicence() throws MDLException {
-        Map<String, List<byte[]>> mockNamespaces =
-                Map.of(
-                        MOCK_NAMESPACE_1, List.of(new byte[] {0x01, 0x02}),
-                        MOCK_NAMESPACE_2, List.of(new byte[] {0x01, 0x02}));
+        String expectedHex = "a10102";
         byte[] mockCborBytes = new byte[] {(byte) 0xA1, 0x01, 0x02};
-
-        when(documentFactory.build(mockNamespaces)).thenReturn(mockMdoc);
-        when(documentFactory.buildAllNamespaces(mockDrivingLicenceDocument))
-                .thenReturn(mockNamespaces);
-        when(cborEncoder.encode(mockMdoc)).thenReturn(mockCborBytes);
+        when(documentFactory.build(mockDrivingLicenceDocument)).thenReturn(mockDocument);
+        when(cborEncoder.encode(mockDocument)).thenReturn(mockCborBytes);
 
         String result =
                 mobileDrivingLicenceService.createMobileDrivingLicence(mockDrivingLicenceDocument);
 
-        assertEquals(
-                EXPECTED_HEX, result, "The actual hex string should match the expected result");
-        verify(documentFactory).buildAllNamespaces(mockDrivingLicenceDocument);
-        verify(documentFactory).build(mockNamespaces);
-        verify(cborEncoder).encode(mockMdoc);
+        assertEquals(expectedHex, result, "The actual hex string should match the expected result");
+        verify(documentFactory).build(mockDrivingLicenceDocument);
+        verify(cborEncoder).encode(mockDocument);
     }
 
     @Test
-    void Should_PropagateException_When_NameSpaceFactoryThrowsMDLException() throws MDLException {
+    void Should_PropagateException_When_DocumentFactoryThrows() throws MDLException {
         MDLException expectedException =
                 new MDLException("Some DocumentFactory error", new RuntimeException());
-        when(documentFactory.buildAllNamespaces(mockDrivingLicenceDocument))
-                .thenThrow(expectedException);
+        when(documentFactory.build(mockDrivingLicenceDocument)).thenThrow(expectedException);
 
         MDLException thrown =
                 assertThrows(
@@ -77,18 +60,15 @@ class MobileDrivingLicenceServiceTest {
                                 mobileDrivingLicenceService.createMobileDrivingLicence(
                                         mockDrivingLicenceDocument));
         assertEquals(expectedException, thrown);
-        verify(documentFactory, never()).build(any());
         verify(cborEncoder, never()).encode(any());
     }
 
     @Test
-    void Should_PropagateException_When_CBOREncoderThrowsMDLException() throws MDLException {
-        Map<String, List<byte[]>> namespaces = Map.of();
+    void Should_PropagateException_When_CBOREncoderThrows() throws MDLException {
         MDLException expectedException =
                 new MDLException("Some CBOREncoder error", new RuntimeException());
-        when(documentFactory.buildAllNamespaces(mockDrivingLicenceDocument)).thenReturn(namespaces);
-        when(documentFactory.build(any())).thenReturn(mockMdoc);
-        when(cborEncoder.encode(mockMdoc)).thenThrow(expectedException);
+        when(documentFactory.build(any())).thenReturn(mockDocument);
+        when(cborEncoder.encode(mockDocument)).thenThrow(expectedException);
 
         MDLException thrown =
                 assertThrows(
@@ -97,6 +77,6 @@ class MobileDrivingLicenceServiceTest {
                                 mobileDrivingLicenceService.createMobileDrivingLicence(
                                         mockDrivingLicenceDocument));
         assertEquals(expectedException, thrown);
-        verify(cborEncoder).encode(mockMdoc);
+        verify(cborEncoder).encode(mockDocument);
     }
 }
