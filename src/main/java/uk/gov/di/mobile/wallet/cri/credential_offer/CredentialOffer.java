d0ffed2f24b9f729bbe0d913d94dbe82bc2e712a
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-09 21:58:51 +0100

    refactor: document code, use Map.of() instead of verbose HashMap building, use constants

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential_offer/CredentialOffer.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential_offer/CredentialOffer.java
@@ -5,12 +5,31 @@ import lombok.Getter;
 
 import java.util.Map;
 
+/**
+ * Represents a credential offer according to the OpenID for Verifiable Credentials specification.
+ * This object contains information about the credential issuer, the types of credentials being
+ * offered, and the authorization grants available to obtain those credentials.
+ */
+@Getter
 public class CredentialOffer {
 
+    /** The URL of the credential issuer */
+    @JsonProperty("credential_issuer")
     private final String credentialIssuer;
-    @Getter private final String[] credentials;
-    @Getter private final Map<String, Map<String, String>> grants;
 
+    /** Array of credential types being offered */
+    private final String[] credentials;
+
+    /** Map of authorization grants with their parameters */
+    private final Map<String, Map<String, String>> grants;
+
+    /**
+     * Constructs a new CredentialOffer.
+     *
+     * @param credentialIssuer The URL of the credential issuer.
+     * @param credentialType The type of credential being offered.
+     * @param grants The authorization grants available for obtaining the credential.
+     */
     public CredentialOffer(
             String credentialIssuer,
             String credentialType,
@@ -18,10 +37,5 @@ public class CredentialOffer {
         this.credentialIssuer = credentialIssuer;
         this.credentials = new String[] {credentialType};
         this.grants = grants;
-    }
-
-    @JsonProperty("credential_issuer")
-    public String getCredentialIssuer() {
-        return credentialIssuer;
     }
 }
