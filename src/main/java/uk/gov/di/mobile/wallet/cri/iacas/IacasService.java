c6287258074f33f7a349f01c29663a78a7ca4e55
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-17 18:55:50 +0100

    feat: build IACAs from PEM certificate

--- src/main/java/uk/gov/di/mobile/wallet/cri/iacas/IacasService.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/iacas/IacasService.java
@@ -1,66 +1,38 @@
 package uk.gov.di.mobile.wallet.cri.iacas;
 
-import com.nimbusds.jose.jwk.ECKey;
-import jakarta.xml.bind.DatatypeConverter;
 import uk.gov.di.mobile.wallet.cri.services.ConfigurationService;
 import uk.gov.di.mobile.wallet.cri.services.object_storage.ObjectStore;
 
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateEncodingException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
+import java.util.List;
 
 import static uk.gov.di.mobile.wallet.cri.util.ArnUtil.extractCertificateId;
 
 public class IacasService {
 
-  private final ConfigurationService configurationService;
-  private final ObjectStore objectStore;
+    private final ConfigurationService configurationService;
+    private final ObjectStore objectStore;
 
-  public IacasService(ConfigurationService configurationService, ObjectStore objectStore) {
-    this.configurationService = configurationService;
-    this.objectStore = objectStore;
-  }
+    /**
+     * Constructs the IacasService with required dependencies.
+     *
+     * @param configurationService Provides configuration values.
+     * @param objectStore Provides access to the object storage.
+     */
+    public IacasService(ConfigurationService configurationService, ObjectStore objectStore) {
+        this.configurationService = configurationService;
+        this.objectStore = objectStore;
+    }
 
-  public String getCertificateDetails() throws Exception {
-    String bucketName = configurationService.getCertificatesBucketName();
-    String certificateAuthorityArn = configurationService.getCertificateAuthorityArn();
+    public Iacas getIacas() throws Exception {
+        String bucketName = configurationService.getCertificatesBucketName();
+        String certificateAuthorityArn = configurationService.getCertificateAuthorityArn();
 
-    String rootCertificateId = extractCertificateId(certificateAuthorityArn);
+        String rootCertificateId = extractCertificateId(certificateAuthorityArn);
+        String objectKey = rootCertificateId + "/certificate.pem";
+        String certificatePem = objectStore.getObject(bucketName, objectKey);
 
-    String objectKey = rootCertificateId + "/certificate.pem";
+        Iaca iaca = Iaca.fromCertificate(rootCertificateId, true, certificatePem);
 
-    byte[] pemBytes = objectStore.getObject(bucketName, objectKey);
-    String pem = new String(pemBytes, StandardCharsets.UTF_8);
-
-    CertificateFactory factory = CertificateFactory.getInstance("X.509");
-    ByteArrayInputStream is = new ByteArrayInputStream(pem.getBytes(StandardCharsets.UTF_8));
-    X509Certificate cert = (X509Certificate) factory.generateCertificate(is);
-//
-//    Iaca iaca = Iaca.fromCertificate(rootCertificateId,true, pem);
-//    List<Iaca> iacaList = new ArrayList<>();
-//    iacaList.add(iaca);
-//
-//    Iacas iacas = Iacas.fromList(iacaList);
-
-    return "Subject: " + cert.getSubjectX500Principal() + "\n" +
-            "Issuer: " + cert.getIssuerX500Principal() + "\n" +
-            "Valid From: " + cert.getNotBefore() + "\n" +
-            "Valid To: " + cert.getNotAfter() + "\n" +
-            "Public Key: " + cert.getPublicKey() + "\n" +
-            "Thumbprint: " + getThumbprint(cert) + "\n" +
-            "Signature Algorithm: " + cert.getSigAlgName() + "\n";
-
-  }
-
-
-  private static String getThumbprint(X509Certificate cert)
-          throws NoSuchAlgorithmException, CertificateEncodingException {
-    MessageDigest md = MessageDigest.getInstance("SHA-1");
-    md.update(cert.getEncoded());
-    return DatatypeConverter.printHexBinary(md.digest()).toLowerCase();
-  }
+        return new Iacas(List.of(iaca));
+    }
 }
