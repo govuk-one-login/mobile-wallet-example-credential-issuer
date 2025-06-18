3e60a2df3b9da4f66d8e186a66109165d4dd1cd5
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-18 12:17:33 +0100

    feat: set up s3 with localstack

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -23,9 +23,22 @@ aws --endpoint-url=http://localhost:4566 kms create-alias \
     --alias-name alias/localSigningKeyAlias \
     --target-key-id ff275b92-0def-4dfc-b0f6-87c96b26c6c7
 
-aws --endpoint-url=http://localhost:4566 s3api create-bucket \
-    --region eu-west-2 \
-    --bucket certificates
+aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket certificates --create-bucket-configuration LocationConstraint=eu-west-2 --region eu-west-2
 
-aws --endpoint-url=http://localhost:4566 s3 cp s3://certificates/6bb42872-f4ed-4d55-a937-b8ffb8760de4/certificate.pem ./certificate.pem \
-    --region eu-west-2
+cat <<EOF > certificate.pem
+-----BEGIN CERTIFICATE-----
+MIIB8TCCAZegAwIBAgIQLmndcaaE19g+3lJupyQCojAKBggqhkjOPQQDAjA/MQsw
+CQYDVQQGEwJVSzEwMC4GA1UEAwwnbURMIEV4YW1wbGUgSUFDQSBSb290IC0gREVW
+IGVudmlyb25tZW50MB4XDTI1MDQxNTA5MTQyMFoXDTM0MDQxNjEwMTQyMFowPzEL
+MAkGA1UEBhMCVUsxMDAuBgNVBAMMJ21ETCBFeGFtcGxlIElBQ0EgUm9vdCAtIERF
+ViBlbnZpcm9ubWVudDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABBV+UF7RuBa4
+gu0aVFRRD1plr+Bnu1dsv9eNXbU2ZqGq0FkM4IknCZ12Y/zENDVA8VyM+hNRlkvr
+SZMoqCSnobejdTBzMBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYDVR0OBBYEFHomsAv2
+hf6lUS/4necL1PqJLNEJMA8GA1UdDwEB/wQFAwMHBgAwLQYDVR0SBCYwJIYiaHR0
+cHM6Ly9tb2JpbGUuZGV2LmFjY291bnQuZ292LnVrLzAKBggqhkjOPQQDAgNIADBF
+AiBFqaelXoq3kySjLkoy6cbnv5mFfUjyFN9emgHyWcy2OgIhAOtizfGVyHNAQ2wD
+z6mnTX/lWqYiEThH9Gb3xRXKrslN
+-----END CERTIFICATE-----
+EOF
+
+aws --endpoint-url=http://localhost:4566 s3 cp certificate.pem s3://certificates/6bb42872-f4ed-4d55-a937-b8ffb8760de4/ --region eu-west-2
