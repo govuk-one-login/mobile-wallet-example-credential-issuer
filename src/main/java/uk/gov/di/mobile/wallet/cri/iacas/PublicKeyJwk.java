78d87e157f8f1d6db258231ef20057aff2a79df2
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-18 12:43:32 +0100

    feat: change PublicKeyJwk to be a record and test it

--- src/main/java/uk/gov/di/mobile/wallet/cri/iacas/PublicKeyJwk.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/iacas/PublicKeyJwk.java
@@ -1,28 +1,11 @@
 package uk.gov.di.mobile.wallet.cri.iacas;
 
-import lombok.Getter;
-import lombok.Setter;
-
-/** Represents the IACA's Elliptic Curve Public Key in JWK (JSON Web Key) format. */
-@Getter
-@Setter
-public class PublicKeyJwk {
-    /** The key type (e.g., "EC" for Elliptic Curve). */
-    private String kty;
-
-    /** The curve used for the elliptic curve key (e.g., "P-256"). */
-    private String crv;
-
-    /** The x-coordinate for the EC public key. */
-    private String x;
-
-    /** The y-coordinate for the EC public key. */
-    private String y;
-
-    public PublicKeyJwk(String kty, String crv, String x, String y) {
-        this.kty = kty;
-        this.crv = crv;
-        this.x = x;
-        this.y = y;
-    }
-}
+/**
+ * Represents the IACA's Elliptic Curve Public Key in JWK (JSON Web Key) format.
+ *
+ * @param kty The key type (e.g., "EC" for Elliptic Curve).
+ * @param crv The curve used for the elliptic curve key (e.g., "P-256").
+ * @param x The x-coordinate for the EC public key.
+ * @param y The y-coordinate for the EC public key.
+ */
+public record PublicKeyJwk(String kty, String crv, String x, String y) {}
