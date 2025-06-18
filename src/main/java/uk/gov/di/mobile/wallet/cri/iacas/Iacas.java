7152f9da118190e374c23bc599f7fb155beee917
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-18 12:56:49 +0100

    feat: change Iaca to be a record and test it

--- src/main/java/uk/gov/di/mobile/wallet/cri/iacas/Iacas.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/iacas/Iacas.java
@@ -1,18 +1,10 @@
 package uk.gov.di.mobile.wallet.cri.iacas;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-/** Represents a list of IACA (Issuing Authority Certificate Authority) certificates. */
-@Getter
-@Setter
-public class Iacas {
-    /** The list of IACA certificates. */
-    private List<Iaca> data;
-
-    public Iacas(List<Iaca> data) {
-        this.data = data;
-    }
-}
+/**
+ * Represents a list of IACA (Issuing Authority Certificate Authority) certificates.
+ *
+ * @param data The list of IACA certificates.
+ */
+public record Iacas(List<Iaca> data) {}
