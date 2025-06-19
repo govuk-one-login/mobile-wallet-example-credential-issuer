c52b0731c260d21bd0458e0778ffc89f7130cffb
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-19 18:04:56 +0100

    feat: add ErrorMessages class to hold all error messages

---
+++ src/main/java/uk/gov/di/mobile/wallet/cri/responses/ErrorMessages.java
@@ -0,0 +1,8 @@
+package uk.gov.di.mobile.wallet.cri.responses;
+
+public class ErrorMessages {
+    public static final String INVALID_NOTIFICATION_ID = "invalid_notification_id";
+    public static final String INVALID_NOTIFICATION_REQUEST = "invalid_notification_request";
+    public static final String INVALID_TOKEN = "invalid_token";
+    public static final String INVALID_PROOF = "invalid_proof";
+}
