b68ee15f3e7d1998f2720e874b63ae7754ef8e61
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-23 12:37:14 +0100

    build: create Document Signing Key 1 and alias with LocalStack

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -22,3 +22,14 @@ aws --endpoint-url=http://localhost:4566 kms create-alias \
     --region eu-west-2 \
     --alias-name alias/localSigningKeyAlias \
     --target-key-id ff275b92-0def-4dfc-b0f6-87c96b26c6c7
+
+aws --endpoint-url=http://localhost:4566 kms create-key \
+    --region eu-west-2 \
+    --key-usage SIGN_VERIFY \
+    --key-spec ECC_NIST_P256 \
+    --tags '[{"TagKey":"_custom_id_","TagValue":"1291b7bc-3d2c-47f0-a52a-cb6cb0fba6b4"}]'
+
+aws --endpoint-url=http://localhost:4566 kms create-alias \
+    --region eu-west-2 \
+    --alias-name alias/documentSigningKeyAlias1 \
+    --target-key-id 1291b7bc-3d2c-47f0-a52a-cb6cb0fba6b4
