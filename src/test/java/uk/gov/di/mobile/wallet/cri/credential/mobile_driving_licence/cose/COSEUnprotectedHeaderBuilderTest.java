8679040566e2c42511867883c08f0f9c5b3fab71
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-27 14:33:46 +0100

    test: write unit tests for COSEUnprotectedHeaderBuilder

--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEUnprotectedHeaderBuilderTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEUnprotectedHeaderBuilderTest.java
@@ -1,4 +1,25 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cose;
 
-public class COSEUnprotectedHeaderBuilderTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class COSEUnprotectedHeaderBuilderTest {
+
+    @Test
+    void Should_BuildHeaderWithX5Chain_When_X5ChainSet() {
+        Object x5chainValue = "dummy-certificate-chain";
+        COSEUnprotectedHeaderBuilder builder = new COSEUnprotectedHeaderBuilder();
+        builder.x5chain(x5chainValue);
+
+        COSEUnprotectedHeader header = builder.build();
+
+        assertNotNull(header);
+        Map<Integer, Object> map = header.unprotectedHeader();
+        assertEquals(1, map.size());
+        assertEquals(x5chainValue, map.get(33));
+    }
 }
