94fd24cf09b487ffb6b9e360bd84bf770aaef118
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-30 17:45:49 +0100

    refactor: simplify Namespaces class and make it a record

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/Namespaces.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/Namespaces.java
@@ -1,17 +1,9 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Namespaces {
-    private final Map<String, List<IssuerSignedItem>> namespaces;
-
-    public Namespaces(Map<String, List<IssuerSignedItem>> namespaces) {
-        this.namespaces = Collections.unmodifiableMap(new LinkedHashMap<>(namespaces));
-    }
-
+public record Namespaces(Map<String, List<IssuerSignedItem>> namespaces) {
     public Map<String, List<IssuerSignedItem>> asMap() {
         return namespaces;
     }
