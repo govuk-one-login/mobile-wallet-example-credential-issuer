6c0d43534245e7c5530c04c0812db072b7b5968b
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-30 15:39:03 +0100

    style: reformat code

--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEProtectedHeaderBuilderTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEProtectedHeaderBuilderTest.java
@@ -4,31 +4,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class COSEProtectedHeaderBuilderTest {
 
-  private COSEProtectedHeaderBuilder builder;
+    private COSEProtectedHeaderBuilder builder;
 
-  @BeforeEach
-  void setUp() {
-    builder = new COSEProtectedHeaderBuilder();
-  }
+    @BeforeEach
+    void setUp() {
+        builder = new COSEProtectedHeaderBuilder();
+    }
 
-  @Test
-  void Should_BuildCOSEProtectedHeader() {
-    int algorithm = -7; // ES256 algorithm
-    COSEProtectedHeader header = builder.alg(algorithm).build();
+    @Test
+    void Should_BuildCOSEProtectedHeader() {
+        int algorithm = -7; // ES256 algorithm
+        COSEProtectedHeader header = builder.alg(algorithm).build();
 
-    assertNotNull(header);
-    assertEquals(algorithm, header.protectedHeader().get(1));
-  }
+        assertNotNull(header);
+        assertEquals(algorithm, header.protectedHeader().get(1));
+    }
 
-  @Test
-  void Should_ThrowIllegalArgumentException_When_X5chainIsNotSet() {
-    IllegalStateException exception = assertThrows(IllegalStateException.class,
-            () -> builder.build());
-    assertEquals("alg must be set", exception.getMessage());
-  }
+    @Test
+    void Should_ThrowIllegalArgumentException_When_X5chainIsNotSet() {
+        IllegalStateException exception =
+                assertThrows(IllegalStateException.class, () -> builder.build());
+        assertEquals("alg must be set", exception.getMessage());
+    }
 }
--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEUnprotectedHeaderBuilderTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEUnprotectedHeaderBuilderTest.java
@@ -8,32 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class COSEUnprotectedHeaderBuilderTest {
 
-  private COSEUnprotectedHeaderBuilder builder;
+    private COSEUnprotectedHeaderBuilder builder;
 
-  @BeforeEach
-  void setUp() {
-    builder = new COSEUnprotectedHeaderBuilder();
-  }
+    @BeforeEach
+    void setUp() {
+        builder = new COSEUnprotectedHeaderBuilder();
+    }
 
-  @Test
-  void Should_BuildCOSEUnprotectedHeader() {
-    byte[] x5chain = new byte[] {1, 2, 3};
+    @Test
+    void Should_BuildCOSEUnprotectedHeader() {
+        byte[] x5chain = new byte[] {1, 2, 3};
 
-    COSEUnprotectedHeader header = builder.x5chain(x5chain).build();
+        COSEUnprotectedHeader header = builder.x5chain(x5chain).build();
 
-    assertNotNull(header);
-    assertEquals(x5chain, header.unprotectedHeader().get(33));
-  }
+        assertNotNull(header);
+        assertEquals(x5chain, header.unprotectedHeader().get(33));
+    }
 
-  @Test
-  void Should_ThrowIllegalArgumentException_When_X5chainIsNull() {
-    assertThrows(IllegalArgumentException.class, () -> builder.x5chain(null));
-  }
+    @Test
+    void Should_ThrowIllegalArgumentException_When_X5chainIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> builder.x5chain(null));
+    }
 
-  @Test
-  void Should_ThrowIllegalArgumentException_When_X5chainIsNotSet() {
-    IllegalStateException exception = assertThrows(IllegalStateException.class,
-            () -> builder.build());
-    assertEquals("x5chain must be set", exception.getMessage());
-  }
+    @Test
+    void Should_ThrowIllegalArgumentException_When_X5chainIsNotSet() {
+        IllegalStateException exception =
+                assertThrows(IllegalStateException.class, () -> builder.build());
+        assertEquals("x5chain must be set", exception.getMessage());
+    }
 }
