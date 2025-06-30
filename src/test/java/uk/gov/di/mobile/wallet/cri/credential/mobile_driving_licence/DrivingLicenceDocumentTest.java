7b58868547af37b205f47cc472da4749212f79dd
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-06-30 22:12:49 +0100

    chore: remove wildcard imports

--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/DrivingLicenceDocumentTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/DrivingLicenceDocumentTest.java
@@ -9,8 +9,12 @@ import java.time.Period;
 import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 class DrivingLicenceDocumentTest {
 
--- src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSESign1Test.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSESign1Test.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class COSESign1Test {
 
