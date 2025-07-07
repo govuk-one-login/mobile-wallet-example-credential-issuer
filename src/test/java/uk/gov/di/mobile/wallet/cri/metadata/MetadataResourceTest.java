b1b2b7ca5e0544b989f7fed38adaf960e3007f9f
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-07-07 19:55:50 +0100

    test: update MetadataResource unit tests

--- src/test/java/uk/gov/di/mobile/wallet/cri/metadata/MetadataResourceTest.java
+++ src/test/java/uk/gov/di/mobile/wallet/cri/metadata/MetadataResourceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.di.mobile.wallet.cri.metadata;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
@@ -9,6 +8,7 @@ import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.di.mobile.wallet.cri.services.ConfigurationService;
 
@@ -17,6 +17,7 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.RETURNS_SELF;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
@@ -25,6 +26,7 @@ class MetadataResourceTest {
 
     private final ConfigurationService configurationService = mock(ConfigurationService.class);
     private final MetadataBuilder metadataBuilder = mock(MetadataBuilder.class, RETURNS_SELF);
+    private final Metadata metadata = mock(Metadata.class);
     private final ResourceExtension resource =
             ResourceExtension.builder()
                     .addResource(new MetadataResource(configurationService, metadataBuilder))
@@ -35,22 +37,49 @@ class MetadataResourceTest {
         when(configurationService.getOneLoginAuthServerUrl())
                 .thenReturn("https://test-authorization-server.gov.uk");
         when(configurationService.getSelfUrl()).thenReturn("https://test-credential-issuer.gov.uk");
+        when(configurationService.getEnvironment()).thenReturn("test");
     }
 
     @Test
     void Should_Return200AndMetadata() throws IOException {
-        Metadata mockCriMetadata = getMockCRIMetadata();
-        when(metadataBuilder.build()).thenReturn(mockCriMetadata);
+        when(metadataBuilder.build()).thenReturn(metadata);
 
         final Response response =
                 resource.target("/.well-known/openid-credential-issuer").request().get();
 
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode expectedResponseBody =
-                objectMapper.readTree(objectMapper.writeValueAsString(mockCriMetadata));
+                objectMapper.readTree(objectMapper.writeValueAsString(metadata));
         JsonNode actualResponseBody = objectMapper.readTree(response.readEntity(String.class));
         assertEquals(200, response.getStatus());
         assertEquals(expectedResponseBody, actualResponseBody);
+    }
+
+    @Test
+    void Should_CallMetadataBuilderWithIACAsBuildURL_When_EnvironmentIsStaging() {
+        when(metadataBuilder.build()).thenReturn(metadata);
+        when(configurationService.getEnvironment()).thenReturn("staging");
+
+        final Response response =
+                resource.target("/.well-known/openid-credential-issuer").request().get();
+
+        assertEquals(200, response.getStatus());
+        verify(metadataBuilder, Mockito.times(1))
+                .setIacasEndpoint(
+                        "https://example-credential-issuer.mobile.build.account.gov.uk/iacas");
+    }
+
+    @Test
+    void Should_CallMetadataBuilderWithIACAsTestURL_When_EnvironmentIsNotStaging() {
+        when(metadataBuilder.build()).thenReturn(metadata);
+        when(configurationService.getEnvironment()).thenReturn("test");
+
+        final Response response =
+                resource.target("/.well-known/openid-credential-issuer").request().get();
+
+        assertEquals(200, response.getStatus());
+        verify(metadataBuilder, Mockito.times(1))
+                .setIacasEndpoint("https://test-credential-issuer.gov.uk/iacas");
     }
 
     @Test
@@ -61,21 +90,5 @@ class MetadataResourceTest {
                 resource.target("/.well-known/openid-credential-issuer").request().get();
 
         assertEquals(500, response.getStatus());
-    }
-
-    private static Metadata getMockCRIMetadata() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        Object credentialConfigurationsSupported =
-                objectMapper.readValue(
-                        "{\"SocialSecurityCredential\":{\"format\":\"jwt_vc_json\",\"credential_definition\":{\"types\":[\"VerifiableCredential\",\"SocialSecurityCredential\"]},\"cryptographic_binding_methods_supported\":[\"did:key\"],\"credential_signing_alg_values_supported\":[\"ES256\"],\"proof_types_supported\":{\"jwt\":{\"proof_signing_alg_values_supported\":[\"ES256\"]}}},\"BasicDisclosureCredential\":{\"format\":\"jwt_vc_json\",\"credential_definition\":{\"types\":[\"VerifiableCredential\",\"BasicDisclosureCredential\"]},\"cryptographic_binding_methods_supported\":[\"did:key\"],\"credential_signing_alg_values_supported\":[\"ES256\"],\"proof_types_supported\":{\"jwt\":{\"proof_signing_alg_values_supported\":[\"ES256\"]}}},\"DigitalVeteranCard\":{\"format\":\"jwt_vc_json\",\"credential_definition\":{\"type\":[\"VerifiableCredential\",\"DigitalVeteranCard\"]},\"cryptographic_binding_methods_supported\":[\"did:key\"],\"credential_signing_alg_values_supported\":[\"ES256\"],\"proof_types_supported\":{\"jwt\":{\"proof_signing_alg_values_supported\":[\"ES256\"]}}},\"org.iso.18013.5.1.mDL\": {\"format\": \"mso_mdoc\",\"doctype\": \"org.iso.18013.5.1.mDL\",\"cryptographic_binding_methods_supported\": [\"cose_key\"],\"credential_signing_alg_values_supported\": [\"ES256\"]}}",
-                        Object.class);
-        return new Metadata(
-                "https://test-credential-issuer.gov.uk",
-                "https://test-authorization-server.gov.uk",
-                "https://test-credential-issuer.gov.uk/credential",
-                "https://test-credential-issuer.gov.uk/notification",
-                "https://test-credential-issuer.gov.uk/iacas",
-                credentialConfigurationsSupported);
     }
 }
