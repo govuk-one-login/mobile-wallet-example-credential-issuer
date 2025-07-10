56968d7b4a25f0d86fc511fc5dd3dd9eaf5f453a
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-07-10 14:53:17 +0100

    refactor: move COSEKey record

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DeviceKeyInfo.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DeviceKeyInfo.java
@@ -1,5 +1,7 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
+import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cose.COSEKey;
+
 public record DeviceKeyInfo(COSEKey deviceKey, KeyAuthorizations keyAuthorizations
-        //        KeyInfo keyInfo
+                            //        KeyInfo keyInfo
         ) {}
