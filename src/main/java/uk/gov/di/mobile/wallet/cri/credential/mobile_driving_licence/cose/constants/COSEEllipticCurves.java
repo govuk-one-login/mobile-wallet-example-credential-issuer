e0f3ab29b14d337ada56ad110cfcbfeffc066e75
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-07-11 17:23:21 +0100

    fix: remove typo in COSEEllipticCurves

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/constants/COSEEllipticCurves.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/constants/COSEEllipticCurves.java
@@ -1,7 +1,7 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cose.constants;
 
 public final class COSEEllipticCurves {
-    /** P-256 (-v1) */
+    /** P-256 (-1) */
     public static final int P256 = -1;
 
     private COSEEllipticCurves() {}
