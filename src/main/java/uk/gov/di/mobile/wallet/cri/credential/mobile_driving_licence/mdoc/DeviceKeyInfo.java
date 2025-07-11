918e8546367df8468b4a6ddf704e975330f0e264
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-07-11 17:25:40 +0100

    style: format code

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DeviceKeyInfo.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DeviceKeyInfo.java
@@ -3,5 +3,5 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cose.COSEKey;
 
 public record DeviceKeyInfo(COSEKey deviceKey, KeyAuthorizations keyAuthorizations
-                            //        KeyInfo keyInfo
+        //        KeyInfo keyInfo
         ) {}
