7ea18d13919ca433ca9ce464f1729ed62c87bea3
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-07-14 16:05:20 +0100

    feat: remove KeyInfo from DeviceKeyInfo

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DeviceKeyInfo.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/DeviceKeyInfo.java
@@ -2,6 +2,4 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cose.COSEKey;
 
-public record DeviceKeyInfo(COSEKey deviceKey, KeyAuthorizations keyAuthorizations
-        //        KeyInfo keyInfo
-        ) {}
+public record DeviceKeyInfo(COSEKey deviceKey, KeyAuthorizations keyAuthorizations) {}
