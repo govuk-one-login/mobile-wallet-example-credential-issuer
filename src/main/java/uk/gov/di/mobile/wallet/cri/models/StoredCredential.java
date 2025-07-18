33bc656dbb3dc9c47c41cc1d334e34ef73808b78
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-07-18 17:16:37 +0100

    feat: Add StoredCredential model

---
+++ src/main/java/uk/gov/di/mobile/wallet/cri/models/StoredCredential.java
@@ -0,0 +1,31 @@
+package uk.gov.di.mobile.wallet.cri.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@Getter
+@Setter
+@DynamoDbBean
+public class StoredCredential {
+
+    String credentialIdentifier;
+    String notificationId;
+    Long timeToLive;
+
+    public StoredCredential() {
+        // Empty constructor needed for dynamoDb deserialization
+    }
+
+    public StoredCredential(String credentialIdentifier, String notificationId, Long timeToLive) {
+        this.credentialIdentifier = credentialIdentifier;
+        this.notificationId = notificationId;
+        this.timeToLive = timeToLive;
+    }
+
+    @DynamoDbPartitionKey
+    public String getCredentialIdentifier() {
+        return credentialIdentifier;
+    }
+}
