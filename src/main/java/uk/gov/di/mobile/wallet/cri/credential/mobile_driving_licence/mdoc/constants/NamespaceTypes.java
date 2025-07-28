ad8e76d2b5783c0d5e965fb8dc5808a78f2ce847
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-07-28 12:09:01 +0100

    fix: change mDL domestic namespace to org.iso.18013.5.1.GB

--- src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/constants/NamespaceTypes.java
+++ src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/constants/NamespaceTypes.java
@@ -5,7 +5,7 @@ public final class NamespaceTypes {
     public static final String ISO = "org.iso.18013.5.1";
 
     /** The domestic (GB) namespace for Mobile Driving Licence documents. */
-    public static final String UK = "org.iso.18013.5.1.GB";
+    public static final String GB = "org.iso.18013.5.1.GB";
 
     private NamespaceTypes() {}
 }
