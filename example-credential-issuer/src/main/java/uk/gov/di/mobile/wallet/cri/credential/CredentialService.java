cc9a72c85d59c2f75f36a78d76d5ff509fd2e82a
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-08-13 14:10:06 +0100

    fix: remove the DynamoDb update permission

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/CredentialService.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/CredentialService.java
@@ -106,7 +106,7 @@ public class CredentialService {
                 documentId);
 
         dataStore.deleteCredentialOffer(
-                credentialOfferId); // Delete credential offer after being redeemed
+                credentialOfferId); // Delete credential offer after being redeemed to prevent replay
 
         String sub = proofJwtData.didKey();
         String vcType = document.getVcType();
