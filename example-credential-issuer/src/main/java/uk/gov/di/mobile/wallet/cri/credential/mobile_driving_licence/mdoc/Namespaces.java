5a49d0a078c28c658de5cd0b50ddfb65d97aa641
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-08-28 10:11:37 +0100

    refactor: remove redundant method in Namespaces record

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/Namespaces.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/Namespaces.java
@@ -3,8 +3,4 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 import java.util.List;
 import java.util.Map;
 
-public record Namespaces(Map<String, List<IssuerSignedItem>> namespaces) {
-    public Map<String, List<IssuerSignedItem>> namespaces() {
-        return namespaces;
-    }
-}
+public record Namespaces(Map<String, List<IssuerSignedItem>> namespaces) {}
