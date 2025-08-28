ec696f9147b41666776918f114dd669e170dcb21
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-08-28 13:08:31 +0100

    feat: remove newly added serializers

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cbor/JacksonCBOREncoderProvider.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cbor/JacksonCBOREncoderProvider.java
@@ -1,21 +1,16 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cbor;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import uk.gov.di.mobile.wallet.cri.annotations.ExcludeFromGeneratedCoverageReport;
-import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.DeviceKeyInfo;
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.IssuerSigned;
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.IssuerSignedItem;
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.MobileSecurityObject;
-import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.ValidityInfo;
 
 import java.time.Instant;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
 
 /** Provides a pre-configured {@link CBORMapper} instance with custom serializers. */
 public final class JacksonCBOREncoderProvider {
@@ -25,21 +20,12 @@ public final class JacksonCBOREncoderProvider {
         throw new IllegalStateException("Instantiation is not valid for this class.");
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
     public static CBORMapper configuredCBORMapper() {
         SimpleModule simpleModule =
                 new SimpleModule()
-                        .addSerializer(
-                                (Class) Map.class,
-                                (JsonSerializer) new DefiniteLengthMapSerializer<>())
-                        .addSerializer(
-                                (Class) List.class,
-                                (JsonSerializer) new DefiniteLengthListSerializer<>())
                         .addSerializer(LocalDate.class, new LocalDateCBORSerializer())
                         .addSerializer(Instant.class, new InstantCBORSerializer())
                         .addSerializer(IssuerSignedItem.class, new IssuerSignedItemCBORSerializer())
-                        .addSerializer(DeviceKeyInfo.class, new DeviceKeyInfoCBORSerializer())
-                        .addSerializer(ValidityInfo.class, new ValidityInfoCBORSerializer())
                         .addSerializer(
                                 MobileSecurityObject.class, new MobileSecurityObjectSerializer())
                         .addSerializer(IssuerSigned.class, new IssuerSignedCBORSerializer());
