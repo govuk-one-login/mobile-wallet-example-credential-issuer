2a0539e1211325dbb05740f795bc9e522193f396
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-09-09 11:16:28 +0100

    feat: inject clock in ExpiryUtil to properly test it

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/CredentialExpiryCalculator.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/CredentialExpiryCalculator.java
@@ -1,44 +1,52 @@
 package uk.gov.di.mobile.wallet.cri.credential;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import uk.gov.di.mobile.wallet.cri.credential.basic_check_credential.BasicCheckDocument;
 import uk.gov.di.mobile.wallet.cri.credential.digital_veteran_card.VeteranCardDocument;
 import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.DrivingLicenceDocument;
 import uk.gov.di.mobile.wallet.cri.credential.social_security_credential.SocialSecurityDocument;
 import uk.gov.di.mobile.wallet.cri.util.ExpiryUtil;
 
+import java.time.Clock;
+
 public class CredentialExpiryCalculator implements ExpiryCalculator {
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper =
+            new ObjectMapper()
+                    .registerModule(new JavaTimeModule())
+                    .registerModule(new Jdk8Module());
+    ;
+    ExpiryUtil expiryUtil = new ExpiryUtil(Clock.systemDefaultZone());
 
     @Override
     public long calculateExpiry(Document document) {
         String vcType = document.getVcType();
-        System.out.println(vcType);
 
         CredentialType credentialType = CredentialType.fromType(vcType);
         return switch (credentialType) {
             case SOCIAL_SECURITY_CREDENTIAL -> {
                 SocialSecurityDocument socialSecurityDocument =
                         mapper.convertValue(document.getData(), SocialSecurityDocument.class);
-                yield ExpiryUtil.calculateExpiryTimeFromTtl(
+                yield expiryUtil.calculateExpiryTimeFromTtl(
                         socialSecurityDocument.getCredentialTtlMinutes());
             }
             case BASIC_DISCLOSURE_CREDENTIAL -> {
                 BasicCheckDocument basicCheckDocument =
                         mapper.convertValue(document.getData(), BasicCheckDocument.class);
-                yield ExpiryUtil.calculateExpiryTimeFromTtl(
+                yield expiryUtil.calculateExpiryTimeFromTtl(
                         basicCheckDocument.getCredentialTtlMinutes());
             }
             case DIGITAL_VETERAN_CARD -> {
                 VeteranCardDocument veteranCardDocument =
                         mapper.convertValue(document.getData(), VeteranCardDocument.class);
-                yield ExpiryUtil.calculateExpiryTimeFromTtl(
+                yield expiryUtil.calculateExpiryTimeFromTtl(
                         veteranCardDocument.getCredentialTtlMinutes());
             }
             case MOBILE_DRIVING_LICENCE -> {
                 DrivingLicenceDocument drivingLicenceDocument =
                         mapper.convertValue(document.getData(), DrivingLicenceDocument.class);
-                yield ExpiryUtil.calculateExpiryTimeFromDate(
+                yield expiryUtil.calculateExpiryTimeFromDate(
                         drivingLicenceDocument.getExpiryDate());
             }
         };
