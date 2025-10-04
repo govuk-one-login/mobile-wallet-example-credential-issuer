c9aace2004f73d75066e1e3d66a922d7eeb733d2
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-10-04 14:53:59 +0100

    fix: code smell fix

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/revoke/RevokeService.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/revoke/RevokeService.java
@@ -1,16 +1,11 @@
 package uk.gov.di.mobile.wallet.cri.revoke;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import uk.gov.di.mobile.wallet.cri.notification.NotificationService;
 import uk.gov.di.mobile.wallet.cri.services.data_storage.DataStore;
 import uk.gov.di.mobile.wallet.cri.services.data_storage.DataStoreException;
 
 public class RevokeService {
 
     private final DataStore dataStore;
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
 
     public RevokeService(DataStore dataStore) {
         this.dataStore = dataStore;
