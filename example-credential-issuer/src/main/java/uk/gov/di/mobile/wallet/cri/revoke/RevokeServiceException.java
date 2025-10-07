8687c03b0cba7760e89319b50f47c83d59ce689f
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-10-07 17:04:24 +0100

    feat: delete RevokeServiceException as not required in this PR

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/revoke/RevokeServiceException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.di.mobile.wallet.cri.revoke;
-
-public class RevokeServiceException extends RuntimeException {
-    public RevokeServiceException(String message) {
-        super(message);
-    }
-}
