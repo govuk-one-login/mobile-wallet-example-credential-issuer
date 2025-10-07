5d6a92fb4f111f725dc3ce38d9206e5de8d91db9
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-10-07 17:03:27 +0100

    feat: simplify RevokeRequestBody

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/revoke/RevokeRequestBody.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/revoke/RevokeRequestBody.java
@@ -1,18 +1,8 @@
 package uk.gov.di.mobile.wallet.cri.revoke;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class RevokeRequestBody {
-    @JsonProperty("drivingLicenceNumber")
     private String drivingLicenceNumber;
-
-    @JsonCreator
-    public RevokeRequestBody(
-            @JsonProperty(value = "drivingLicenceNumber", required = true)
-                    String drivingLicenceNumber) {
-        this.drivingLicenceNumber = drivingLicenceNumber;
-    }
 }
