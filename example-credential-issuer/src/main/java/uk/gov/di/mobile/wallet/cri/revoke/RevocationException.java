aea501cd7c49ce23b42d5a20df2c53fe1b3938e0
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-10-17 15:57:13 +0100

    feat: add second constructor to RevocationException

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/revoke/RevocationException.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/revoke/RevocationException.java
@@ -5,4 +5,8 @@ public class RevocationException extends Exception {
     public RevocationException(String message) {
         super(message);
     }
+
+    public RevocationException(String message, Exception exception) {
+        super(message, exception);
+    }
 }
