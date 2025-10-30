1ddcae51985f6750c381554271a3b1ffdd472774
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-10-30 17:42:54 +0000

    chore: replace underscore with hyphen in localstack container name

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/services/ConfigurationService.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/services/ConfigurationService.java
@@ -52,7 +52,7 @@ public class ConfigurationService extends Configuration {
      * @return The LocalStack endpoint URL
      */
     public String getLocalstackEndpoint() {
-        return "http://localstack_credentialIssuer:4566";
+        return "http://localstack-credentialIssuer:4566";
     }
 
     /**
