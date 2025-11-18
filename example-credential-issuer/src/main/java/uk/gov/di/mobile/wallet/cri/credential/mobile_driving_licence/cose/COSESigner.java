6f64c73e891e2dbe0d3e69a04cd4ff87ea9d24d1
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-11-18 09:52:40 +0000

    feat: change COSEUnprotectedHeader to be a class containing x5chain

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSESigner.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSESigner.java
@@ -58,7 +58,7 @@ public class COSESigner {
 
         return new COSESign1Builder()
                 .protectedHeader(protectedHeaderEncoded)
-                .unprotectedHeader(unprotectedHeader.unprotectedHeader())
+                .unprotectedHeader(unprotectedHeader)
                 .payload(payload)
                 .signature(signature)
                 .build();
--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEUnprotectedHeaderBuilder.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEUnprotectedHeaderBuilder.java
@@ -1,23 +1,20 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cose;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class COSEUnprotectedHeaderBuilder {
-    private final Map<Integer, Object> unprotectedHeader = new LinkedHashMap<>();
+    byte[] x5chain;
 
     public COSEUnprotectedHeaderBuilder x5chain(byte[] x5chain) {
         if (x5chain == null) {
             throw new IllegalArgumentException("x5chain cannot be null");
         }
-        unprotectedHeader.put(33, x5chain);
+        this.x5chain = x5chain;
         return this;
     }
 
     public COSEUnprotectedHeader build() {
-        if (!unprotectedHeader.containsKey(33)) {
+        if (this.x5chain == null) {
             throw new IllegalStateException("x5chain must be set");
         }
-        return new COSEUnprotectedHeader(new LinkedHashMap<>(unprotectedHeader));
+        return new COSEUnprotectedHeader(this);
     }
 }
