c41601272ff3cd6981d185fb64647fa5209f921c
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-11-18 10:17:18 +0000

    feat: apply snake_case naming strategy to Status record

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/Status.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/Status.java
@@ -1,3 +1,7 @@
 package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
-public record Status(StatusList status_list) {}
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record Status(StatusList statusList) {}
