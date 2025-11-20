97f182a827846419a078d35cd2750126339549c7
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-11-20 15:13:08 +0000

    feat: override equals, hashCode and toString in IssuerSignedItem

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/IssuerSignedItem.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/mdoc/IssuerSignedItem.java
@@ -2,14 +2,44 @@ package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@SuppressWarnings("java:S6218")
+import java.util.Arrays;
+import java.util.Objects;
+
 public record IssuerSignedItem(
-        /*
-        "Equals method should be overridden in records containing array fields java:S6218"
-        Overriding the equals method is not required as byte[] random is created within this class
-        and can only contain bytes
-        */
         @JsonProperty("digestID") Integer digestId,
         byte[] random,
         String elementIdentifier,
-        Object elementValue) {}
+        Object elementValue) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IssuerSignedItem that)) return false;
+        return Objects.equals(digestId, that.digestId)
+                && Arrays.equals(random, that.random)
+                && Objects.equals(elementIdentifier, that.elementIdentifier)
+                && Objects.equals(elementValue, that.elementValue);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(digestId, elementIdentifier, elementValue);
+        result = 31 * result + Arrays.hashCode(random);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "IssuerSignedItem{"
+                + "digestId="
+                + digestId
+                + ", random="
+                + Arrays.toString(random)
+                + ", elementIdentifier='"
+                + elementIdentifier
+                + '\''
+                + ", elementValue="
+                + elementValue
+                + '}';
+    }
+}
