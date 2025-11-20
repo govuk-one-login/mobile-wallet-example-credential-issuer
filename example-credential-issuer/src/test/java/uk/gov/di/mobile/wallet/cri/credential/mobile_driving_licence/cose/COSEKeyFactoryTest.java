97cfca2a8c7defa5f0ae002ca9c6ac86332072d7
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-11-20 16:13:52 +0000

    test: simplify tests in COSEKeyFactoryTest

--- example-credential-issuer/src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEKeyFactoryTest.java
+++ example-credential-issuer/src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cose/COSEKeyFactoryTest.java
@@ -12,8 +12,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.ECGenParameterSpec;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -28,42 +26,29 @@ class COSEKeyFactoryTest {
 
     @Test
     void Should_ConvertP256KeyToCOSEKey() throws Exception {
-        byte[] expectedY = {
-            -101, -83, 16, -38, 66, -12, 45, -72, 71, 74, 91, 92, 41, -118, -53, -93, 75, 53, -25,
-            117, -24, -57, -7, -109, -26, -12, 84, -50, 12, 124, 50, 65
-        };
-        byte[] expectedX = {
-            37, -110, 113, 59, -70, 27, 71, -62, 44, -65, -97, 57, 18, -56, -96, -47, -77, -12, 0,
-            111, -24, 75, 61, 60, 71, 95, -82, 82, -106, -37, -4, -127
-        };
         ECPublicKey ecPublicKey = EcKeyHelper.getEcKey().toECPublicKey();
 
         COSEKey coseKey = coseKeyFactory.fromECPublicKey(ecPublicKey);
 
-        assertAll(
-                () -> assertEquals(COSEKeyTypes.EC2, coseKey.keyType(), "Key type should be EC2"),
-                () ->
-                        assertEquals(
-                                COSEEllipticCurves.P256, coseKey.curve(), "Curve should be P-256"),
-                () -> assertEquals(32, coseKey.x().length, "x must be 32 bytes for P-256"),
-                () -> assertEquals(32, coseKey.y().length, "y must be 32 bytes for P-256"),
-                () -> assertArrayEquals(expectedX, coseKey.x(), "x must match expected value"),
-                () -> assertArrayEquals(expectedY, coseKey.y(), "y must match expected value"));
+        assertEquals(COSEKeyTypes.EC2, coseKey.keyType(), "Key type should be EC2");
+        assertEquals(COSEEllipticCurves.P256, coseKey.curve(), "Curve should be P-256");
+        assertEquals(32, coseKey.x().length, "x must be 32 bytes for P-256");
+        assertEquals(32, coseKey.y().length, "y must be 32 bytes for P-256");
     }
 
     @Test
     void Should_ThrowIllegalArgumentException_When_NonP256Curve()
             throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
-        kpg.initialize(new ECGenParameterSpec("secp384r1")); // P-384 curve
+        kpg.initialize(new ECGenParameterSpec("secp384r1")); // non P-256 curve
         ECPublicKey ecPublicKey = (ECPublicKey) kpg.generateKeyPair().getPublic();
 
-        IllegalArgumentException ex =
+        IllegalArgumentException exception =
                 assertThrows(
                         IllegalArgumentException.class,
-                        () -> new COSEKeyFactory().fromECPublicKey(ecPublicKey));
+                        () -> coseKeyFactory.fromECPublicKey(ecPublicKey));
         assertEquals(
                 "Invalid EC key curve: expected P-256 (secp256r1), got field size 384 bits",
-                ex.getMessage());
+                exception.getMessage());
     }
 }
