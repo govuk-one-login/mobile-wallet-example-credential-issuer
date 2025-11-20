a166fc7ff18419f9b77b35055999ff41a7276bd7
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-11-20 13:35:11 +0000

    feat(revert): delete new DrivingPrivilegeSerializer
    This reverts commit 167d7274ad764daf1183b777dcb4dbcb8ffdfa9a.

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cbor/JacksonCBOREncoderProvider.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cbor/JacksonCBOREncoderProvider.java
@@ -22,6 +22,7 @@ public final class JacksonCBOREncoderProvider {
                         .addSerializer(new IssuerSignedItemCBORSerializer())
                         .addSerializer(new MobileSecurityObjectSerializer())
                         .addSerializer(new IssuerSignedCBORSerializer())
+                        .addSerializer(new DrivingPrivilegeSerializer())
                         .addSerializer(new ValidityInfoSerializer())
                         .addSerializer(new StatusSerializer())
                         .addSerializer(new DeviceKeyInfoSerializer())
