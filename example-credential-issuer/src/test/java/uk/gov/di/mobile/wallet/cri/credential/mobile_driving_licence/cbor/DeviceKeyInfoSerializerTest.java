96968fc610ed9345f3290eb48f293c102659ae03
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-11-20 09:56:29 +0000

    test: add tests for DeviceKeyInfoSerialize

---
+++ example-credential-issuer/src/test/java/uk/gov/di/mobile/wallet/cri/credential/mobile_driving_licence/cbor/DeviceKeyInfoSerializerTest.java
@@ -0,0 +1,84 @@
+package uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cbor;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.dataformat.cbor.CBORGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.cose.COSEKey;
+import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.DeviceKeyInfo;
+import uk.gov.di.mobile.wallet.cri.credential.mobile_driving_licence.mdoc.KeyAuthorizations;
+
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class DeviceKeyInfoSerializerTest {
+
+    @Mock private CBORGenerator cborGenerator;
+    @Mock private SerializerProvider serializerProvider;
+
+    @Test
+    void Should_SerializeDeviceKeyInfo() throws IOException {
+        COSEKey coseKey = new COSEKey(2, 1, new byte[] {0x01}, new byte[] {0x02});
+        Set<String> namespaces = new LinkedHashSet<>();
+        namespaces.add("org.iso.18013.5.1");
+        namespaces.add("org.iso.18013.5.1.GB");
+        DeviceKeyInfo valueToSerialize =
+                new DeviceKeyInfo(coseKey, new KeyAuthorizations(namespaces));
+
+        new DeviceKeyInfoSerializer()
+                .serialize(valueToSerialize, cborGenerator, serializerProvider);
+
+        InOrder inOrder = inOrder(cborGenerator);
+        inOrder.verify(cborGenerator).writeStartObject(2);
+        inOrder.verify(cborGenerator).writeFieldName("deviceKey");
+        inOrder.verify(cborGenerator).writeStartObject(4);
+        inOrder.verify(cborGenerator).writeFieldId(1);
+        inOrder.verify(cborGenerator).writeNumber(2);
+        inOrder.verify(cborGenerator).writeFieldId(-1);
+        inOrder.verify(cborGenerator).writeNumber(1);
+        inOrder.verify(cborGenerator).writeFieldId(-2);
+        inOrder.verify(cborGenerator).writeBinary(new byte[] {0x01});
+        inOrder.verify(cborGenerator).writeFieldId(-3);
+        inOrder.verify(cborGenerator).writeBinary(new byte[] {0x02});
+        inOrder.verify(cborGenerator).writeEndObject();
+        inOrder.verify(cborGenerator).writeFieldName("keyAuthorizations");
+        inOrder.verify(cborGenerator).writeStartObject(1);
+        inOrder.verify(cborGenerator).writeFieldName("nameSpaces");
+        inOrder.verify(cborGenerator).writeStartArray(any(), eq(2));
+        inOrder.verify(cborGenerator).writeString("org.iso.18013.5.1");
+        inOrder.verify(cborGenerator).writeString("org.iso.18013.5.1.GB");
+        inOrder.verify(cborGenerator).writeEndArray();
+        inOrder.verify(cborGenerator, times(2)).writeEndObject();
+    }
+
+    @Test
+    void Should_ThrowIllegalArgumentException_When_SerializerIsNonCBORGenerator() {
+        JsonGenerator invalidGenerator = mock(JsonGenerator.class);
+        DeviceKeyInfo value =
+                new DeviceKeyInfo(
+                        new COSEKey(2, 1, new byte[] {0x01}, new byte[] {0x02}),
+                        new KeyAuthorizations(Set.of("ns")));
+
+        IllegalArgumentException exception =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () ->
+                                new DeviceKeyInfoSerializer()
+                                        .serialize(value, invalidGenerator, serializerProvider));
+        assertEquals("Requires CBORGenerator", exception.getMessage());
+    }
+}
