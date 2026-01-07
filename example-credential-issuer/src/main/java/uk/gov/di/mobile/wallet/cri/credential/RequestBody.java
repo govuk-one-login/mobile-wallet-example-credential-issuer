82d45bb74473f697ae8faf03a8db935dcd9fd97e
nstefanopoulou-gds <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-01-07 15:48:16 +0000

    docs: added a comment for the content of token response in request body (#417)
    * docs: Added a comment for credential request body
    
    * docs: Added a comment for credential request body in the RequestBody.java
    
    * style: Apply Spotless formatting
    
    * docs: added a comment for the content of token response in request body

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/RequestBody.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/credential/RequestBody.java
@@ -5,6 +5,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import uk.gov.di.mobile.wallet.cri.credential.proof.Proof;
 
+/*
+As per the OID4VCI specification, the request body should include either
+'credential_configuration_id' or 'credential_identifier',
+depending on what is returned in the response body from the authorization server token response.
+However, the One Login authorization server works slightly differently
+and does not return those values in the token response body.
+Instead, it returns 'credential_identifiers' (an array containing one 'credential_identifier')
+as part of the access token payload. Since the Wallet includes this access token in the
+credential request, the credential can be uniquely identified,
+making it unnecessary to include these parameters in the request body.
+https://govukverify.atlassian.net/browse/DCMAW-13558?focusedCommentId=279751
+*/
+
 @Getter
 public class RequestBody {
 
