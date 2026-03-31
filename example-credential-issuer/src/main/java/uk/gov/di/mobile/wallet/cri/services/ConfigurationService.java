ee80dcda8813177ece1d57e2bd407df9800d0fea
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2026-03-31 15:16:22 +0100

    chore: remove debug log

--- example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/services/ConfigurationService.java
+++ example-credential-issuer/src/main/java/uk/gov/di/mobile/wallet/cri/services/ConfigurationService.java
@@ -34,7 +34,6 @@ public class ConfigurationService extends Configuration {
      * @return The current environment name
      */
     public String getEnvironment() {
-        System.out.println("ENV: " + getEnvOrDefault("ENVIRONMENT", "local"));
         return getEnvOrDefault("ENVIRONMENT", "local");
     }
 
