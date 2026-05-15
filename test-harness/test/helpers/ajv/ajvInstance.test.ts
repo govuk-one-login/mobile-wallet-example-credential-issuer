da5c839190572f64b69ed304af24043531b3b7e5
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-05-15 11:10:23 +0100

    chore: test pre-commit

--- test-harness/test/helpers/ajv/ajvInstance.test.ts
+++ test-harness/test/helpers/ajv/ajvInstance.test.ts
@@ -22,7 +22,7 @@ describe("getAjvInstance", () => {
       type: "object",
       properties: {
         name: { type: "string", minLength: 1 },
-        age: {
+        age: { type: "number", minimum: 0 },
       },
       required: ["name", "age"],
     };
