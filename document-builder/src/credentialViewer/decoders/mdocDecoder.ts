b387809a88d73af42fb8009886641a414cb48b9f
Shubhankar Choubey <shubhankar.choubey@digital.cabinet-office.gov.uk>
2026-05-15 16:56:22 +0100

    chore(document-builder): validate user input before sinking

--- document-builder/src/credentialViewer/decoders/mdocDecoder.ts
+++ document-builder/src/credentialViewer/decoders/mdocDecoder.ts
@@ -45,6 +45,7 @@ export function decodeMDocCredential(credential: string): {
     Buffer.from(credentialSignature.payload),
   );
 
+  // test
   return {
     credentialClaims,
     credentialSignature,
