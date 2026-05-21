3d63b29c22503b02ea016609c0b6df19a2f39c3c
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2026-05-21 16:31:08 +0100

    test(document-builder): add tests for calculateCredentialTtlSeconds

---
+++ document-builder/test/utils/calculateCredentialTtlSeconds.test.ts
@@ -0,0 +1,15 @@
+import { calculateCredentialTtlSeconds } from "../../src/utils/calculateCredentialTtlSeconds";
+
+describe("calculateCredentialTtlSeconds", () => {
+  beforeEach(() => {
+    jest.useFakeTimers().setSystemTime(new Date("2025-05-02T00:00:00Z"));
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("should return the seconds between now and the expiry date", () => {
+    expect(calculateCredentialTtlSeconds("02", "05", "2026")).toBe(31532400);
+  });
+});
