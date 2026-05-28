6dc547c596ecdbbc61eed66b86a1c68310e9deb2
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-05-28 17:18:32 +0100

    feat: add expectedUpdate in driving licence data

--- document-builder/src/types/DrivingLicenceData.ts
+++ document-builder/src/types/DrivingLicenceData.ts
@@ -19,4 +19,5 @@ export interface DrivingLicenceData {
   driving_privileges: DrivingPrivilege[];
   un_distinguishing_sign: string;
   provisional_driving_privileges?: DrivingPrivilege[];
+  expectedUpdate?: number;
 }
