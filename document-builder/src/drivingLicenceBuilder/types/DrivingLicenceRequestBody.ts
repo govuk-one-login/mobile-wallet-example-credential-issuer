07377f22efca079a08bdca0128ef77c445285dcb
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-05-29 11:22:33 +0100

    feat: add expectedUpdateDays in body for VC and mdl

--- document-builder/src/drivingLicenceBuilder/types/DrivingLicenceRequestBody.ts
+++ document-builder/src/drivingLicenceBuilder/types/DrivingLicenceRequestBody.ts
@@ -39,5 +39,6 @@ export interface DrivingLicenceRequestBody {
   "credentialExpiry-day": string;
   "credentialExpiry-month": string;
   "credentialExpiry-year": string;
+  "expectedUpdateDays"?: string;
   throwError: string;
 }
