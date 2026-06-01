5d6f6c5628b197aa0a7866057619499c0ec21074
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2026-06-01 12:20:40 +0100

    feat(test-harness): add expectedUpdate validation to JWT credential payload

--- test-harness/test/helpers/credential/jwt/payloadSchema.ts
+++ test-harness/test/helpers/credential/jwt/payloadSchema.ts
@@ -62,6 +62,10 @@ export const payloadSchema = {
       type: "string",
       pattern: String.raw`^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d)|3[01]T([01]\d|2[0-3]):[0-5]\d:[0-5]\dZ$`,
     },
+    expectedUpdate: {
+      type: "string",
+      pattern: String.raw`^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d)|3[01]T([01]\d|2[0-3]):[0-5]\d:[0-5]\dZ$`,
+    },
     credentialSubject: {
       type: "object",
       properties: {
