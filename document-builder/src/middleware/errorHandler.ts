6f6bc9c52dda1c4d219b94c56629b57c912b6439
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-07-14 15:50:54 +0100

    refactor: add errorHandler file

---
+++ document-builder/src/middleware/errorHandler.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { logger } from "./logger";
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  logger.error(err.message);
+  res.status(500).send("500.njk");
+}
