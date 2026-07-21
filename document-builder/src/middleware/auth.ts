f919993f7f2fc3f68b75c79c5a1a32c8f07fb794
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-07-21 14:53:33 +0100

    chore: update auth.ts and validateCredentialTypePath and their tests

--- document-builder/src/middleware/auth.ts
+++ document-builder/src/middleware/auth.ts
@@ -9,11 +9,7 @@ export function auth(configuration: AuthMiddlewareConfiguration) {
       req.oidc = await getOIDCClient(configuration);
       next();
     } catch (error) {
-      logger.error(error, "Error building OIDC Client");
-      res.render("500.njk", {
-        errorMessage:
-          "Failed to connect to authentication provider. Please try again later.",
-      });
+      next(new Error("Error building OIDC Client", { cause: error }));
     }
   };
 }
