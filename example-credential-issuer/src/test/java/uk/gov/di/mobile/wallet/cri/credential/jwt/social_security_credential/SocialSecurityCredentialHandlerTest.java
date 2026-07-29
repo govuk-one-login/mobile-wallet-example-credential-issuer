d450a693fcee4f57bfb019ef2d5172a3a671321d
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-07-29 16:54:29 +0100

    refactor(example-cri): use @InjectMocks in SocialSecurityCredentialHandlerTest

--- example-credential-issuer/src/test/java/uk/gov/di/mobile/wallet/cri/credential/jwt/social_security_credential/SocialSecurityCredentialHandlerTest.java
+++ example-credential-issuer/src/test/java/uk/gov/di/mobile/wallet/cri/credential/jwt/social_security_credential/SocialSecurityCredentialHandlerTest.java
@@ -1,9 +1,9 @@
 package uk.gov.di.mobile.wallet.cri.credential.jwt.social_security_credential;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -37,18 +37,13 @@ class SocialSecurityCredentialHandlerTest {
     @Mock private ProofJwtService.ProofJwtData mockProofData;
     @Mock private SocialSecurityDocument mockSocialSecurityDocument;
     @Mock private SocialSecurityCredentialSubject mockCredentialSubject;
-    private SocialSecurityCredentialHandler handler;
+    @InjectMocks private SocialSecurityCredentialHandler handler;
 
     private static final String EXPECTED_CREDENTIAL = "signed-jwt-credential-string";
     private static final String DID_KEY = "did:key:test123";
     private static final long TTL_SECONDS = 1440L;
     private static final Optional<StatusListClient.StatusListInformation> STATUS_LIST_INFORMATION =
             Optional.empty();
-
-    @BeforeEach
-    void setUp() {
-        handler = new SocialSecurityCredentialHandler(mockCredentialBuilder);
-    }
 
     @Test
     void Should_ReturnSocialSecurityCredential() throws SigningException {
